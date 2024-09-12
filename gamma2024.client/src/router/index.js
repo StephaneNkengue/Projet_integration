4ecfe43d38ffa5ad19f47f4f9205eca5afe4d607
ThiMaiTrang <2162067@CegepGranby.qc.ca>
2024-09-12 09:34:16 -0400

    Suppression des fichiers non utiles
    Résolution des problèmes en lien avec les fichiers supprimés

--- gamma2024.client/src/router/index.js
+++ gamma2024.client/src/router/index.js
@@ -1,24 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Accueil from '@/views/Accueil.vue'
-import Connexion from '@/views/Connexion.vue' // Assurez-vous d'avoir créé ce composant
+import BarreDeNavigation from '@/components/BarreDeNavigation.vue'
 
 const routes = [
-    {
-        path: '/',
-        name: 'Accueil',
-        component: Accueil
-    },
-    {
-        path: '/accueil',
-        name: 'AccueilPage',
-        component: Accueil
-    },
-    {
-        path: '/connexion',
-        name: 'Connexion',
-        component: Connexion
-    }
-    // ... autres routes
+    //{
+    //    path: '/',
+    //    name: 'Accueil',
+    //    component: BarreDeNavigation
+    //},
+    //{
+    //    path: '/accueil',
+    //    name: 'AccueilPage',
+    //    component: BarreDeNavigation
+    //}
 ]
 
 const router = createRouter({
