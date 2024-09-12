937fb726b6e892a2927183ca83f551176603567f
MarcAndreLaplante <0930810@cegepgranby.qc.ca>
2024-09-12 14:47:58 -0400

    Boîte de connexion avec noms et boîtes d'entrées.

--- gamma2024.client/src/components/Connexion.vue
+++ gamma2024.client/src/components/Connexion.vue
@@ -1,5 +1,31 @@
 <template>
-    <p>Ceci est la page de connexion.</p>
+    <div class="container d-flex flex-column justify-content-start align-items-stretch container">
+        <h2 class="fs-1 text-center fw-bold mt-5">Connexion</h2>
+        <p class="text-center">Se connecter pour continuer</p>
+
+        <div class="d-flex flex-column justify-content-start align-items-stretch">
+            <div class="px-4 py-2 mb-4">
+                <div class="d-flex flex-row justify-content-center mb-3">
+                    <div class="form-group" style="width: 25%;">
+                        <label for="nomutilisateur" class="ms-3">Nom d'utilisateur</label>
+                        <input type="text" class="form-control" id="nomutilisateur" />
+                    </div>
+                </div>
+                <div class="d-flex flex-row justify-content-center mb-3">
+                    <div class="form-group" style="width: 25%;">
+                        <label for="motdepasse" class="ms-3">Mot de passe</label>
+                        <input type="text" class="form-control" id="motdepasse" />
+                    </div>
+                </div>
+                <div class="d-flex flex-row justify-content-center mb-3">
+                    <div class="form-group" style="width: 25%;">
+                        <p class="ms-3 text-center">J'ai oublié mon Mot de passe</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </template>
 <script setup>
 
