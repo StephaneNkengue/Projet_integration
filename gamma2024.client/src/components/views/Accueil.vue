258f013c46ac0af5da98e32022d6d34d8f516c81
ThiMaiTrang <2162067@CegepGranby.qc.ca>
2024-09-12 16:07:19 -0400

    Ajustement de l'affichage du texte dans la composante Accueil

--- gamma2024.client/src/components/views/Accueil.vue
+++ gamma2024.client/src/components/views/Accueil.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="d-flex flex-column align-items-center">
         <h1>Les Encans de Nantes</h1>
+
         <h2>Encans d'oeuvres d'art</h2>
+
         <p class="w-50 justifyText">
             Les Encans de Nantes au Québec est une entreprise spécialisée dans la gestion d'encans virtuels, axée principalement sur la vente d'œuvres d'art. Elle est co-propriétée par Frédérique Nantes, en collaboration avec deux autres associées, Monique Inverness et Patricia DeNice. L'entreprise est située à Stanstead, dans un bâtiment annexé à la bibliothèque Haskell, un lieu historique partagé entre le Canada et les États-Unis.
-
-            <router-link to="/" class="text-decoration-none text-black align-items-end">
-                <a>Lire plus -></a>
-            </router-link>
         </p>
+
+        <router-link to="/" class="text-decoration-none text-black">
+            <a>Lire plus -></a>
+        </router-link>
     </div>
 </template>
 
