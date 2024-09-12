dc0b0a5845fcc167381d972247c004b82475f56e
StephaneNkengue <nkenguestephane0@gmail.com>
2024-09-12 14:06:13 -0400

    Recupération de la navBar

--- gamma2024.client/src/router/index.js
+++ gamma2024.client/src/router/index.js
@@ -1,30 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Accueil from '@/views/Accueil.vue'
 import Inscription from '@/components/Inscription.vue'
-import Connexion from '@/views/Connexion.vue' // Assurez-vous d'avoir créé ce composant
 
 const routes = [
-    {
-        path: '/',
-        name: 'Accueil',
-        component: Accueil
-    },
-    {
-        path: '/accueil',
-        name: 'AccueilPage',
-        component: Accueil
-    },
-    {
-        path: '/connexion',
-        name: 'Connexion',
-        component: Connexion
-    },
+    //{
+    //    path: '/',
+    //    name: 'Accueil',
+    //    component: Accueil
+    //},
+    //{
+    //    path: '/accueil',
+    //    name: 'AccueilPage',
+    //    component: Accueil
+    //},
     {
         path: '/inscription',
         name: 'inscription',
         component: Inscription
     }
-    // ... autres routes
 ]
 
 const router = createRouter({
