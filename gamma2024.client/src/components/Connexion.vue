f8e1cbbdc1d6b9711751b14a9d11c0531e564b67
MarcAndreLaplante <0930810@cegepgranby.qc.ca>
2024-09-12 11:02:28 -0400

    Lien de la page d'accueil vers la page de connexion.

---
+++ gamma2024.client/src/components/Connexion.vue
@@ -0,0 +1,6 @@
+<template>
+    <p>Ceci est la page de connexion.</p>
+</template>
+<script setup>
+
+</script>
--- gamma2024.client/src/router/index.js
+++ gamma2024.client/src/router/index.js
@@ -1,17 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import BarreDeNavigation from '@/components/BarreDeNavigation.vue'
+import Connexion from '@/components/Connexion.vue'
 
 const routes = [
-    //{
-    //    path: '/',
-    //    name: 'Accueil',
-    //    component: BarreDeNavigation
-    //},
-    //{
-    //    path: '/accueil',
-    //    name: 'AccueilPage',
-    //    component: BarreDeNavigation
-    //}
+    {
+        //    path: '/',
+        //    name: 'Accueil',
+        //    component: BarreDeNavigation
+        //},
+        //{
+        //    path: '/accueil',
+        //    name: 'AccueilPage',
+        //    component: BarreDeNavigation
+        //},
+        path: '/connexion',
+        name: 'Connexion',
+        component: Connexion
+    }
 ]
 
 const router = createRouter({
