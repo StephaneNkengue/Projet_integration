2f454f81f19c3774e41f92f8217a393ab16366ab
ThiMaiTrang <2162067@CegepGranby.qc.ca>
2024-09-13 09:51:06 -0400

    Ajout du paragraphe Déroulement d'un encan dans la page d'Accueil
    Résolution des problèmes d'espacements

--- gamma2024.client/src/components/views/Accueil.vue
+++ gamma2024.client/src/components/views/Accueil.vue
@@ -1,16 +1,32 @@
 <template>
     <div class="d-flex flex-column align-items-center">
-        <h1>Les Encans de Nantes</h1>
+        <div class="text-center">
+            <h1>Les Encans de Nantes</h1>
 
-        <h2>Encans d'oeuvres d'art</h2>
+            <h2>Encans d'oeuvres d'art</h2>
+        </div>
 
-        <p class="w-50 justifyText">
-            Les Encans de Nantes au Québec est une entreprise spécialisée dans la gestion d'encans virtuels, axée principalement sur la vente d'œuvres d'art. Elle est co-propriétée par Frédérique Nantes, en collaboration avec deux autres associées, Monique Inverness et Patricia DeNice. L'entreprise est située à Stanstead, dans un bâtiment annexé à la bibliothèque Haskell, un lieu historique partagé entre le Canada et les États-Unis.
-        </p>
+        <div class="w-50 text-center">
+            <p class="justifyText">
+                Les Encans de Nantes au Québec est une entreprise spécialisée dans la gestion d'encans virtuels, axée principalement sur la vente d'œuvres d'art. Elle est co-propriétée par Frédérique Nantes, en collaboration avec deux autres associées, Monique Inverness et Patricia DeNice. L'entreprise est située à Stanstead, dans un bâtiment annexé à la bibliothèque Haskell, un lieu historique partagé entre le Canada et les États-Unis.
+            </p>
 
-        <router-link to="/" class="text-decoration-none text-black">
-            <a>Lire plus -></a>
-        </router-link>
+            <router-link to="/" class="text-decoration-none text-black">
+                <a>Lire plus -></a>
+            </router-link>
+        </div>
+
+        <div class="w-50 pt-3 px-5 mt-4 bleuMoyenFond text-center text-white">
+            Déroulement d'un encan
+
+            <p class="justifyText">
+                Les membres peuvent parcourir les lots d'œuvres d'art, faire des mises et des surenchères pendant la durée de l'encan, avec une soirée de clôture en direct. Pendant cette soirée, chaque lot est soumis à un intervalle de temps avant la fermeture définitive, avec la possibilité d'ajouter une minute en cas de surenchère. Un effet visuel indique les 20 dernières secondes pour chaque lot, et une notification par courriel est envoyée en cas de surenchère ou d'achat conclu.
+            </p>
+
+            <router-link to="/" class="text-decoration-none text-white">
+                <a>Lire plus -></a>
+            </router-link>
+        </div>
     </div>
 </template>
 
