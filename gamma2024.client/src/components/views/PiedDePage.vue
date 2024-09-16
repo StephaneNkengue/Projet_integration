2cf70c31fc67dd36584cac2b246683b3446a70d7
ThiMaiTrang <2162067@CegepGranby.qc.ca>
2024-09-16 10:43:56 -0400

    Modification de l'affichage du pied de page sur mobile

--- gamma2024.client/src/components/views/PiedDePage.vue
+++ gamma2024.client/src/components/views/PiedDePage.vue
@@ -1,12 +1,12 @@
 <template>
-    <footer class="bleuMarinFond text-white py-1 justify-content-between d-flex align-items-center">
+    <footer class="bleuMarinFond text-white py-1 justify-content-between d-flex align-items-center flex-column flex-md-row">
 
-        <p class="col-md-6 mb-0 ps-3">
+        <p class="col-md-5 mb-0 ps-3">
             © Les Encans de Nantes 2024
         </p>
 
-        <div class="col-md-6">
-            <ul class="nav justify-content-end">
+        <div class="col-md-7">
+            <ul class="nav justify-content-end d-flex align-items-center flex-column flex-md-row">
                 <li>
                     <router-link to="/" class="text-decoration-none">
                         <a class="nav-link text-white">
