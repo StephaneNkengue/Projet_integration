9ae53861d60d547766c320424535b9e7789ad435
ThiMaiTrang <2162067@CegepGranby.qc.ca>
2024-09-17 12:21:14 -0400

    Ajout section Associées dans page Contact

--- gamma2024.client/src/components/views/Contact.vue
+++ gamma2024.client/src/components/views/Contact.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="d-flex flex-column align-items-center">
-        <div class="contourDiv w-50 d-flex d-inline gap-5 px-5">
+        <div class="contourDiv w-50 d-flex gap-5 px-5">
             <div class="d-flex flex-column align-items-center justify-content-around">
                 <div class="d-flex flex-column align-items-center">
                     <p class="mb-1">Courriel:</p>
@@ -16,6 +16,16 @@
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1994.6945548108804!2d-72.0976130141144!3d45.00616334838177!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cb66f442b8d2677%3A0xe3542c488d5c1062!2s1%20Church%20St%2C%20Stanstead%2C%20QC%20J0B%203E2!5e0!3m2!1sen!2sca!4v1726587933561!5m2!1sen!2sca" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
+
+        <div class="mt-4 d-flex flex-column align-items-center">
+            <h2>Associées</h2>
+
+            <div class="d-flex gap-3">
+                <p>Monique Inverness</p>
+                <p>Patricia DeNice</p>
+                <p>Frédérique Nantes</p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -23,4 +33,7 @@
 </script>
 
 <style scoped>
+    .test {
+        font-size: 20px;
+    }
 </style>
