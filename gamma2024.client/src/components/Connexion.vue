8155f4fd07482b1c6a009e9fc94928ac89428f58
MarcAndreLaplante <0930810@cegepgranby.qc.ca>
2024-09-17 11:10:52 -0400

    Des classes CSS remplacent les attributs de styles dans les div.

--- gamma2024.client/src/components/Connexion.vue
+++ gamma2024.client/src/components/Connexion.vue
@@ -1,26 +1,26 @@
 <template>
-    <div class="bg-image pt-5" style="background-image: url('/public/images/DessinGris.PNG'); background-size:cover; background-position:0px -100px;position: fixed; height: 100vh; width: 100%;">
-        <div class="container d-flex flex-column justify-content-start align-items-stretch bg-white bg-opacity-75" style="border-radius:15px; overflow: hidden; height: 450px; width: 410px;">
+    <div class="bg-image pt-5 imageDeFondEsquise">
+        <div class="container d-flex flex-column justify-content-start align-items-stretch bg-white bg-opacity-75 cadreBlanc">
             <h2 class="fs-1 text-center fw-bold pt-4">Connexion</h2>
             <p class="text-center">Se connecter pour continuer</p>
 
             <div class="d-flex flex-column justify-content-start align-items-stretch">
                 <div class="px-4 py-2 mb-4">
                     <div class="d-flex flex-row justify-content-center mb-3">
-                        <div class="form-group" style="width: 90%;">
+                        <div class="form-group w-90">
                             <label for="nomutilisateur" class="fw-bold ms-3">Nom d'utilisateur</label>
                             <input type="text" class="form-control" id="nomutilisateur" />
                         </div>
                     </div>
                     <div class="d-flex flex-row justify-content-center mb-3">
-                        <div class="form-group" style="width: 90%;">
+                        <div class="form-group w-90">
                             <label for="motdepasse" class="fw-bold ms-3">Mot de passe</label>
                             <input type="text" class="form-control" id="motdepasse" />
                         </div>
                     </div>
                     <div class="d-flex flex-row justify-content-center mt-4 mb-1">
-                        <div class="form-group" style="width: 80%;">
-                            <router-link to="/" style="text-decoration: none;">
+                        <div class="form-group w-80">
+                            <router-link to="/" class="text-decoration-none">
                                 <p class="text-center lienEnTexteNoir">J'ai oublié mon Mot de passe</p>
                             </router-link>
                         </div>
@@ -31,8 +31,8 @@
                         </router-link>
                     </div>
                     <div class="d-flex flex-row justify-content-center mb-3">
-                        <div class="form-group" style="width: 80%;">
-                            <router-link to="/" style="text-decoration: none;">
+                        <div class="form-group w-80">
+                            <router-link to="/" class="text-decoration-none">
                                 <p class="fs-5 text-center fw-bold lienEnTexteNoir">M'inscrire</p>
                             </router-link>
                         </div>
@@ -47,3 +47,19 @@
 <script setup>
 
 </script>
+<style scoped>
+    .cadreBlanc {
+        border-radius: 15px;
+        overflow: hidden;
+        height: 450px;
+        width: 410px;
+    }
+
+    .w-80 {
+        width: 80%;
+    }
+
+    .w-90 {
+        width: 90%;
+    }
+</style>
