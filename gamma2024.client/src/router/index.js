9ad8c9e12388e0a187651d29b3a586212dd98a2b
RENÉ-MARCELLIN TCHOKOMI <2136882@CegepGranby.qc.ca>
2024-09-18 14:46:41 -0400

    validation inscription

--- gamma2024.client/src/router/index.js
+++ gamma2024.client/src/router/index.js
@@ -1,27 +1,69 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Inscription from '@/components/Inscription.vue'
 
+import BarreDeNavigation from '@/components/BarreDeNavigation.vue'
+import Connexion from '@/components/Connexion.vue'
+import Inscription from '@/components/Inscription.vue'
+//import HomePage from '@/views/Home.vue'
+import LoginPage from '@/views/Login.vue'
+
 const routes = [
-    //{
-    //    path: '/',
-    //    name: 'Accueil',
-    //    component: Accueil
-    //},
-    //{
-    //    path: '/accueil',
-    //    name: 'AccueilPage',
-    //    component: Accueil
-    //},
+  {
+    path: '/',
+    name: 'Home',
+    component: Connexion
+  },
+  // {
+  //   path: '/home',
+  //   name: 'HomePage',
+  //   component: HomePage
+  // },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginPage
+  },
+  {
+    path: '/inscription',
+    name: 'Inscription',
+    component: Inscription
+  },
     {
-        path: '/inscription',
-        name: 'inscription',
-        component: Inscription
+        //    path: '/',
+        //    name: 'Accueil',
+        //    component: BarreDeNavigation
+        //},
+        //{
+        //    path: '/accueil',
+        //    name: 'AccueilPage',
+        //    component: BarreDeNavigation
+        //},
+        path: '/connexion',
+        name: 'Connexion',
+        component: Connexion
     }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    const isLoggedIn = store.state.isLoggedIn
+    const requiredRole = to.meta.requiredRole
+
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (!isLoggedIn) {
+            next('/login')
+        } else if (requiredRole && !store.state.roles.includes(requiredRole)) {
+            next('/unauthorized') // Cr�ez une vue pour les acc�s non autoris�s
+        } else {
+            next()
+        }
+    } else {
+        next()
+    }
 })
 
 export default router
