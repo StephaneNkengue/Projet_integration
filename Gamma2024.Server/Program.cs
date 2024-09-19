e0dfc5b65b41d43c050418b6e91b1963e8c39759
StephaneNkengue <nkenguestephane0@gmail.com>
2024-09-19 13:11:32 -0400

    Modification du program.cs

--- Gamma2024.Server/Program.cs
+++ Gamma2024.Server/Program.cs
@@ -1,8 +1,78 @@
 using Gamma2024.Server.Data;
 using Gamma2024.Server.Models;
-using Microsoft.AspNetCore.Authentication.Negotiate;
+using Gamma2024.Server.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+
+
+//var builder = WebApplication.CreateBuilder(args);
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+//builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+//builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddRoles<IdentityRole>()
+//    .AddEntityFrameworkStores<ApplicationDbContext>()
+//    .AddDefaultTokenProviders();
+
+//// Add services to the container.
+//builder.Services.AddControllers();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+//builder.Services.AddScoped<PasswordHasher<IdentityUser>>();
+
+//builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+//   .AddNegotiate();
+
+//builder.Services.AddAuthorization(options =>
+//{
+//    options.FallbackPolicy = options.DefaultPolicy;
+//});
+
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("VueAppPolicy", builder =>
+//    {
+//        builder.WithOrigins("https://localhost:5173/")
+//               .AllowCredentials()
+//               .AllowAnyHeader()
+//               .AllowAnyMethod();
+//    });
+//});
+
+//var app = builder.Build();
+
+//app.UseDefaultFiles();
+//app.UseStaticFiles();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//    app.UseMigrationsEndPoint();
+//}
+//else
+//{
+//    app.UseExceptionHandler("/Error");
+//    app.UseHsts();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseRouting();
+
+//app.UseAuthentication();
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.MapFallbackToFile("/index.html");
+
+//app.UseCors("VueAppPolicy");
+
+//app.Run();
+
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,45 +82,31 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultTokenProviders();
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
-// Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddScoped<InscriptionService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<PasswordHasher<IdentityUser>>();
-
-builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
-   .AddNegotiate();
-
-builder.Services.AddAuthorization(options =>
-{
-    options.FallbackPolicy = options.DefaultPolicy;
-});
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("VueAppPolicy", builder =>
+    options.AddPolicy("Development", builder =>
     {
-        builder.WithOrigins("https://localhost:5173/")
-               .AllowCredentials()
+        builder.SetIsOriginAllowed(_ => true)
+               .AllowAnyMethod()
                .AllowAnyHeader()
-               .AllowAnyMethod();
+               .AllowCredentials();
     });
 });
 
 var app = builder.Build();
 
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.UseMigrationsEndPoint();
+    app.UseCors("Development");
 }
 else
 {
@@ -59,16 +115,9 @@ else
 }
 
 app.UseHttpsRedirection();
-
 app.UseRouting();
-
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
-app.MapFallbackToFile("/index.html");
-
-app.UseCors("VueAppPolicy");
 
 app.Run();
