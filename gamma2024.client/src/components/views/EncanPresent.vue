14644b6d92678e45204684f1242a1552424146ee
AdaNeil <2135621@CegepGranby.qc.ca>
2024-09-23 09:56:22 -0400

    Ajout du texte en haut de la page

--- gamma2024.client/src/components/views/EncanPresent.vue
+++ gamma2024.client/src/components/views/EncanPresent.vue
@@ -1,4 +1,11 @@
 <template>
+    <div class="d-flex flex-column align-items-center pb-3">
+        <h1>Encan présent</h1>
+        <p>Date de debut de la soirée de cloture</p>
+
+        <br />
+
+    </div>
 </template>
 
 <script></script>
