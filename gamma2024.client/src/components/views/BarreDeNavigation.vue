3eacf27c17f7ce3cc1134850d67095383ba4300e
ThiMaiTrang <2162067@CegepGranby.qc.ca>
2024-09-24 13:48:12 -0400

    Changement de la barre de navigation
    Modification de la barre de navigation en fonction des commentaires de la cliente partie 2

--- gamma2024.client/src/components/views/BarreDeNavigation.vue
+++ gamma2024.client/src/components/views/BarreDeNavigation.vue
@@ -4,11 +4,11 @@
             <nav class="navbar navbar-expand-md bleuMarinSecondaireFond py-0" data-bs-theme="dark">
                 <div class="container-fluid justify-content-between">
                     <router-link to="Accueil" class="text-decoration-none">
-                        <a class="navbar-brand d-flex align-items-center">
+                        <a class="navbar-brand d-flex align-items-center fs-6">
                             <img src="/images/Logo.png"
                                  alt="Les Encans de Nantes"
                                  height="40" />
-                            Les Encans de Nantes
+                            Les Encans de Nantes <br />au Québec
                         </a>
 
                     </router-link>
@@ -51,14 +51,17 @@
                                 </router-link>
                             </li>
 
-                            <li class="nav-item">
-                                <router-link to="Accueil" class="text-decoration-none">
-                                    <a class="nav-link">
-                                        Déroulement d'un encan
-                                    </a>
-                                </router-link>
-                            </li>
-                            <li class="nav-item dropdown" v-if="estAdmin">
+
+                        </ul>
+
+                        <div class="d-flex justify-content-center gap-3">
+                            <router-link to="Accueil" v-if="!estConnecte">
+                                <button class="btn btn-outline bleuMoyenFond text-white py-0 butttonNavBar" type="button">Inscription</button>
+                            </router-link>
+                            <router-link to="Connexion" v-if="!estConnecte">
+                                <button class="btn btn-outline bleuMoyenFond text-white py-0 butttonNavBar" type="button">Connexion</button>
+                            </router-link>
+                            <div class="collapse navbar-collapse dropdown text-white" v-if="estAdmin">
                                 <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Tableau de bord
                                 </a>
@@ -89,17 +92,9 @@
                                         </router-link>
                                     </li>
                                 </ul>
-                            </li>
+                            </div>
 
-                        </ul>
 
-                        <div class="d-flex justify-content-center gap-3">
-                            <router-link to="Accueil" v-if="!estConnecte">
-                                <button class="btn btn-outline bleuMoyenFond text-white py-0 butttonNavBar" type="button">Inscription</button>
-                            </router-link>
-                            <router-link to="Connexion" v-if="!estConnecte">
-                                <button class="btn btn-outline bleuMoyenFond text-white py-0 butttonNavBar" type="button">Connexion</button>
-                            </router-link>
                             <router-link to="Accueil" v-if="estConnecte" class="text-decoration-none text-white d-flex align-items-center gap-3">
                                 <a class="nav-link">USERNAME</a>
                                 <img src="/icons/Avatar.png" alt="Avatar" height="40" />
@@ -109,16 +104,17 @@
                 </div>
             </nav>
 
-            <nav class="navbar navbar-expand-md bleuMarinSecondaireFond py-0" data-bs-theme="dark">
+            <nav class="navbar navbar-expand-md py-0" data-bs-theme="dark">
                 <div class="container-fluid justify-content-center justify-content-md-between d-flex flex-row-reverse ">
                     <form class="d-flex align-items-center">
-                        <input class="form-control me-3 butttonNavBar" data-bs-theme="light" type="search" placeholder="Faire une recherche" aria-label="Search">
+                        <div class="d-flex" v-if="activationRecherche">
+                            <input class="form-control me-3 butttonNavBar" data-bs-theme="light" type="search" placeholder="Faire une recherche" aria-label="Search">
 
-                        <router-link to="Accueil">
-                            <button class="btn btn-outline bleuMoyenFond me-3 text-white butttonNavBar py-0" type="submit">Rechercher</button>
-                        </router-link>
-
-                        <router-link to="Accueil">
+                            <router-link to="Accueil">
+                                <button class="btn btn-outline bleuMoyenFond me-3 text-white butttonNavBar py-0" type="submit">Rechercher</button>
+                            </router-link>
+                        </div>
+                        <router-link to="Accueil" @click="activationRecherche = !activationRecherche">
                             <img src="/icons/IconeRechercheAvanceeBleu.png"
                                  alt="Icon recherche avancée"
                                  height="30"
@@ -135,7 +131,8 @@
     import { ref } from 'vue'
 
     const estConnecte = ref(false);
-    const estAdmin = ref(true);
+    const estAdmin = ref(false);
+    const activationRecherche = ref(false);
 </script>
 
 <style scoped>
