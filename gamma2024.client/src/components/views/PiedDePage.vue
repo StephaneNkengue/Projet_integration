7afef873435b365aaeb52e4e6f7a968af8952d9c
ThiMaiTrang <2162067@CegepGranby.qc.ca>
2024-09-24 13:50:35 -0400

    Ajout d'un onglet dans le pied de page

--- gamma2024.client/src/components/views/PiedDePage.vue
+++ gamma2024.client/src/components/views/PiedDePage.vue
@@ -7,6 +7,13 @@
 
         <div class="col-md-7">
             <ul class="nav justify-content-end d-flex align-items-center flex-column flex-md-row">
+                <li class="nav-item">
+                    <router-link to="Accueil" class="text-decoration-none">
+                        <a class="nav-link text-white">
+                            Déroulement d'un encan
+                        </a>
+                    </router-link>
+                </li>
                 <li>
                     <router-link to="Accueil" class="text-decoration-none">
                         <a class="nav-link text-white">
