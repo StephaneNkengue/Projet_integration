92e5a0ecdb595cf857cd5526d55622fc8da9c91b
AdaNeil <2135621@CegepGranby.qc.ca>
2024-09-25 16:11:06 -0400

    Ajout d'un router-link dans LotTuile afin de pouvoir eventuellement rediriger vers la page de détails du lot

--- gamma2024.client/src/components/views/LotTuile.vue
+++ gamma2024.client/src/components/views/LotTuile.vue
@@ -1,43 +1,46 @@
 <template>
-    <div class="card">
-        <div class="card-body align-items-middle">
-            <div class="d-flex justify-content-between align-items-middle">
-                <p class="text-start mb-0">Lot {{lot.code}}</p>
-                <p class="text-end mb-0">{{lot.artiste}}</p>
+    <router-link class="text-decoration-none" to="/">
+        <div class="card">
+            <div class="card-body align-items-middle">
+                <div class="d-flex justify-content-between align-items-middle">
+                    <p class="text-start mb-0">Lot {{lot.code}}</p>
+                    <p class="text-end mb-0">{{lot.artiste}}</p>
+                </div>
+
+                <div class="d-flex justify-content-between align-items-middle">
+                    <p class="text-start mb-0">{{lot.hauteur}} X {{lot.largeur}}</p>
+
+                    <img src="/icons/IconeLivrable.png"
+                         height="30"
+                         alt="Livrable"
+                         v-if="lot.livrable" />
+
+                    <img src="/icons/IconeNonLivrable.png"
+                         height="30"
+                         alt="Non livrable"
+                         v-else />
+                </div>
+
+                <div class="row justify-content-around align-items-middle">
+                    <img v-bind:src="lot.photos[0].lien"
+                         class="img-fluid my-2 col-8 col-sm-10 col-md-12 "
+                         alt="Image du lot" />
+
+                </div>
+
+                <p class="text-center mb-0">Valeur: {{(lot.valeurEstimeMin).toFixed(0)}}$ - {{(lot.valeurEstimeMax).toFixed(0)}}$</p>
+                <p class="text-center mb-0">Mise actuelle: {{(lot.mise).toFixed(0)}}$</p>
+
+                <div class="d-flex justify-content-around pt-2">
+                    <button type="button" class="btn btn-primary">Miser</button>
+                </div>
+
+
             </div>
-
-            <div class="d-flex justify-content-between align-items-middle">
-                <p class="text-start mb-0">{{lot.hauteur}} X {{lot.largeur}}</p>
-
-                <img src="/icons/IconeLivrable.png"
-                     height="30"
-                     alt="Livrable"
-                     v-if="lot.livrable" />
-
-                <img src="/icons/IconeNonLivrable.png"
-                     height="30"
-                     alt="Non livrable"
-                     v-else />
-            </div>
-
-            <div class="row justify-content-around align-items-middle">
-                <img v-bind:src="lot.photos[0].lien"
-                     class="img-fluid my-2 col-8 col-sm-10 col-md-12 "
-                     alt="Image du lot" />
-
-            </div>
-
-            <p class="text-center mb-0">Valeur: {{(lot.valeurEstimeMin).toFixed(0)}}$ - {{(lot.valeurEstimeMax).toFixed(0)}}$</p>
-            <p class="text-center mb-0">Mise actuelle: {{(lot.mise).toFixed(0)}}$</p>
-
-            <div class="d-flex justify-content-around pt-2">
-                <button type="button" class="btn btn-primary">Miser</button>
-            </div>
-
 
         </div>
 
-    </div>
+    </router-link>
 
 </template>
 
