a8ce50f6b2c39ed9800bb4c5eb05219b84f706a0
AdaNeil <2135621@CegepGranby.qc.ca>
2024-09-25 16:05:47 -0400

    Modification de LotTuile afin de permettre l'affichage des informations d'un objet
    Ajout des icones de livraison
    Modification du code dans LotTuile afin de permettre l'affichage d'un objet
    Creation d'un objet Lot afin de permettre l'affichage

--- gamma2024.client/src/components/views/LotTuile.vue
+++ gamma2024.client/src/components/views/LotTuile.vue
@@ -2,26 +2,33 @@
     <div class="card">
         <div class="card-body align-items-middle">
             <div class="d-flex justify-content-between align-items-middle">
-                <p class="text-start mb-0">Lot #</p>
-                <p class="text-end mb-0">Artiste Name</p>
+                <p class="text-start mb-0">Lot {{lot.code}}</p>
+                <p class="text-end mb-0">{{lot.artiste}}</p>
             </div>
 
             <div class="d-flex justify-content-between align-items-middle">
-                <p class="text-start mb-0">12 X 24</p>
-                <img src="https://placehold.co/30"
+                <p class="text-start mb-0">{{lot.hauteur}} X {{lot.largeur}}</p>
+
+                <img src="/icons/IconeLivrable.png"
                      height="30"
-                     alt="Statut livrable" />
+                     alt="Livrable"
+                     v-if="lot.livrable" />
+
+                <img src="/icons/IconeNonLivrable.png"
+                     height="30"
+                     alt="Non livrable"
+                     v-else />
             </div>
 
             <div class="row justify-content-around align-items-middle">
-                <img src="https://placehold.co/9000"
+                <img v-bind:src="lot.photos[0].lien"
                      class="img-fluid my-2 col-8 col-sm-10 col-md-12 "
                      alt="Image du lot" />
 
             </div>
 
-            <p class="text-center mb-0">Valeur: 1000$ - 2000$</p>
-            <p class="text-center mb-0">Mise actuelle: 1100$</p>
+            <p class="text-center mb-0">Valeur: {{(lot.valeurEstimeMin).toFixed(0)}}$ - {{(lot.valeurEstimeMax).toFixed(0)}}$</p>
+            <p class="text-center mb-0">Mise actuelle: {{(lot.mise).toFixed(0)}}$</p>
 
             <div class="d-flex justify-content-around pt-2">
                 <button type="button" class="btn btn-primary">Miser</button>
@@ -34,6 +41,30 @@
 
 </template>
 
-<script setup></script>
+<script setup>
+    import { ref } from 'vue'
+
+    let lotRecu = {
+        code: "1-a",
+        artiste: "Nom de l'artiste",
+        hauteur: 12,
+        largeur: 13,
+        valeurEstimeMin: 1000.00,
+        valeurEstimeMax: 2000.00,
+        mise: 1100.03,
+        livrable: false,
+        photos: [
+            {
+                lien: "https://placehold.co/9000",
+            },
+            {
+                lien: "https://placehold.co/6000",
+            },
+        ]
+    };
+
+    const lot = ref(lotRecu)
+
+</script>
 
 <style scoped></style>
