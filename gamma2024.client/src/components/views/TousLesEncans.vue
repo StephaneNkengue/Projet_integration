232eff925a8502b2d647755c5f8080a79d6c316f
ThiMaiTrang <2162067@CegepGranby.qc.ca>
2024-09-26 11:07:19 -0400

    Ajout de la fonctionnalité des boutons

--- gamma2024.client/src/components/views/TousLesEncans.vue
+++ gamma2024.client/src/components/views/TousLesEncans.vue
@@ -4,13 +4,13 @@
             Tous les encans
         </h1>
         <div class="d-flex flex-row-reverse w-100 px-4 me-2 gap-2">
-            <button class="d-flex align-items-center text-center rounded bleuMoyenFond text-white contourDiv btnSurvolerBleuMoyenFond">Tous</button>
-            <button class="d-flex align-items-center text-center rounded bleuMoyenFond text-white contourDiv btnSurvolerBleuMoyenFond">100</button>
-            <button class="d-flex align-items-center text-center rounded bleuMoyenFond text-white contourDiv btnSurvolerBleuMoyenFond">50</button>
-            <button class="d-flex align-items-center text-center rounded bleuMoyenFond text-white contourDiv btnSurvolerBleuMoyenFond">20</button>
+            <button class="d-flex align-items-center text-center rounded bleuMoyenFond text-white contourDiv btnSurvolerBleuMoyenFond" @click="encanParPage = 200">Tous</button>
+            <button class="d-flex align-items-center text-center rounded bleuMoyenFond text-white contourDiv btnSurvolerBleuMoyenFond" @click="encanParPage = 100">100</button>
+            <button class="d-flex align-items-center text-center rounded bleuMoyenFond text-white contourDiv btnSurvolerBleuMoyenFond" @click="encanParPage = 50">50</button>
+            <button class="d-flex align-items-center text-center rounded bleuMoyenFond text-white contourDiv btnSurvolerBleuMoyenFond" @click="encanParPage = 20">20</button>
         </div>
         <div class="w-100 px-3 row row-cols-2">
-            <div v-for="index in 15" class="col w-50 py-3">
+            <div v-for="index in encanParPage" class="col w-50 py-3">
                 <AffichageEncanTuile />
             </div>
         </div>
@@ -19,6 +19,10 @@
 
 <script setup>
     import AffichageEncanTuile from '@/components/views/AffichageEncanTuile.vue'
+    import { ref } from 'vue'
+
+    const encanParPage = ref(20);
+
 </script>
 
 <style scoped>
