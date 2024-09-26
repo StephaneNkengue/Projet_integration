d974ff384b01cad96cb22f0dbb6d0a6b2d0b5e0e
ThiMaiTrang <2162067@CegepGranby.qc.ca>
2024-09-26 10:33:03 -0400

    Ajout de l'affichage des encans dans la Page Tous Les Encans
    Ajout des icons de choix de la disposition des encans et des lots

---
+++ gamma2024.client/src/components/views/AffichageEncanTuile.vue
@@ -0,0 +1,50 @@
+<template>
+
+    <div class="contourDiv p-2 align-items-center d-flex d-flex flex-column">
+        <h2>Encan #105</h2>
+        <div class="d-flex w-100 ps-5 mt-3">
+            <div class="col d-flex align-items-center justify-content-center flex-column">
+                <div class="row justify-content-start w-100">
+                    <p>Description : <br />Encan d'oeuvre d'art</p>
+                </div>
+                <div class="row justify-content-start w-100">
+                    <p>Date de début : <br />15 septembre 2024</p>
+                </div>
+                <div class="row justify-content-start w-100">
+                    <p>Date de fin : <br />25 septembre 2024</p>
+                </div>
+            </div>
+            <div class="col d-flex justify-content-center align-items-center">
+                <img v-bind:src="lot.photos[0].lien"
+                     class="img-fluid"
+                     alt="Image d'un lot" />
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+    import { ref } from 'vue'
+
+    let lotRecu = {
+        photos: [
+            {
+                lien: "https://placehold.co/9000",
+            },
+            {
+                lien: "https://placehold.co/6000",
+            },
+        ]
+    };
+
+    const lot = ref(lotRecu)
+
+</script>
+
+<style scoped>
+    img {
+        width: 200px;
+        height: 200px;
+    }
+</style>
--- gamma2024.client/src/components/views/TousLesEncans.vue
+++ gamma2024.client/src/components/views/TousLesEncans.vue
@@ -1,13 +1,24 @@
 <template>
-    <div>
+    <div class="d-flex flex-column align-items-center pb-3">
         <h1>
             Tous les encans
         </h1>
+        <div class="d-flex flex-row-reverse w-100 px-4 me-2 gap-2">
+            <button class="d-flex align-items-center text-center rounded bleuMoyenFond text-white contourDiv btnSurvolerBleuMoyenFond">Tous</button>
+            <button class="d-flex align-items-center text-center rounded bleuMoyenFond text-white contourDiv btnSurvolerBleuMoyenFond">100</button>
+            <button class="d-flex align-items-center text-center rounded bleuMoyenFond text-white contourDiv btnSurvolerBleuMoyenFond">50</button>
+            <button class="d-flex align-items-center text-center rounded bleuMoyenFond text-white contourDiv btnSurvolerBleuMoyenFond">20</button>
+        </div>
+        <div class="w-100 px-3 row row-cols-2">
+            <div v-for="index in 15" class="col w-50 py-3">
+                <AffichageEncanTuile />
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
-
+    import AffichageEncanTuile from '@/components/views/AffichageEncanTuile.vue'
 </script>
 
 <style scoped>
