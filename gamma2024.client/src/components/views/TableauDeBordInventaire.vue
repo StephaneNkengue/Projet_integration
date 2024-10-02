c55450efcb7bffd6890366fca5e33d10fc9ae68c
MarcAndreLaplante <0930810@cegepgranby.qc.ca>
2024-10-02 15:32:46 -0400

    Les colonnes se cachent avec succès avec le dropdown.
    Parcontre, l'onglet Encans ne fonctionne pas et la table ne prend pas toute la largeur disponible.

--- gamma2024.client/src/components/views/TableauDeBordInventaire.vue
+++ gamma2024.client/src/components/views/TableauDeBordInventaire.vue
@@ -2,7 +2,7 @@
     <div class="pt-5">
         <h2>Liste des lots</h2>
         <div class="dropdown">
-            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <button class="btn bleuMoyenFond btnSurvolerBleuMoyenFond text-white dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Sélectionner les colonnes
             </button>
             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
@@ -19,8 +19,8 @@
                     </label>
                 </li>
                 <li>
-                    <input type="checkbox" id="lot#Checkbox" checked>
-                    <label class="form-check-label" for="lot#Checkbox">
+                    <input type="checkbox" id="lotNumeroCheckbox" checked>
+                    <label class="form-check-label" for="lotNumeroCheckbox">
                         Lot #
                     </label>
                 </li>
@@ -86,73 +86,83 @@
                 </li>
             </ul>
         </div>
-        <div>
-            <table class="table table-striped" style="width: 100%;">
-                <colgroup>
-                    <col span="1" id="lotIdColonne" class="lotId"> <!--style="visibility:collapse;"-->
-                    <col span="1" id="lotEncanColonne" class="lotEncan">
-                    <col span="1" id="lot#Colonne" class="lot#">
-                    <col span="1" id="lotPrixOuvertureColonne" class="lotPrixOuverture">
-                    <col span="1" id="lotValeurMinPourVendreColonne" class="lotValeurMinPourVendre">
-                    <col span="1" id="lotEstimationMinColonne" class="lotEstimationMin">
-                    <col span="1" id="lotEstimationMaxColonne" class="lotEstimationMax">
-                    <col span="1" id="lotCategorieColonne" class="lotCategorie">
-                    <col span="1" id="lotArtisteColonne" class="lotArtiste">
-                    <col span="1" id="lotDimensionColonne" class="lotDimension">
-                    <col span="1" id="lotDescriptionColonne" class="lotDescription">
-                    <col span="1" id="lotMediumColonne" class="lotMedium">
-                    <col span="1" id="lotLivraisonColonne" class="lotLivraison">
-                </colgroup>
-                <thead>
-                    <tr>
-                        <th class="lotId">Lot Id</th>
-                        <th class="lotEncan">Encan</th>
-                        <th class="lot#">Lot #</th>
-                        <th class="lotPrixOuverture">Prix Ouverture</th>
-                        <th class="lotValeurMinPourVendre">Valeur Min Pour Vente</th>
-                        <th class="lotEstimationMin">Estimation Min</th>
-                        <th class="lotEstimationMax">Estimation Max</th>
-                        <th class="lotCategorie">Categorie</th>
-                        <th class="lotArtiste">Artiste</th>
-                        <th class="lotDimension">Dimension (en po)</th>
-                        <th class="lotDescription">Description</th>
-                        <th class="lotMedium">Medium</th>
-                        <th class="lotLivraison">Livraison</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td class="lotId">1</td>
-                        <td class="lotEncan">232</td>
-                        <td class="lot#">1</td>
-                        <td class="lotPrixOuverture">200$</td>
-                        <td class="lotValeurMinPourVendre"></td>
-                        <td class="lotEstimationMin">600$</td>
-                        <td class="lotEstimationMax">800$</td>
-                        <td class="lotCategorie">Art-Paintings</td>
-                        <td class="lotArtiste">Guy Paquet</td>
-                        <td class="lotDimension">12 x 16</td>
-                        <td class="lotDescription">1984/La nouvelle pratique</td>
-                        <td class="lotMedium">Huile sur panneau</td>
-                        <td class="lotLivraison">oui</td>
-                    </tr>
-                    <tr>
-                        <td class="lotId">2</td>
-                        <td class="lotEncan">232</td>
-                        <td class="lot#">1a</td>
-                        <td class="lotPrixOuverture">2200$</td>
-                        <td class="lotValeurMinPourVendre">3800$</td>
-                        <td class="lotEstimationMin">3500$</td>
-                        <td class="lotEstimationMax">5000$</td>
-                        <td class="lotCategorie">Art-Paintings</td>
-                        <td class="lotArtiste">Vladimir Horik</td>
-                        <td class="lotDimension">20 x 30</td>
-                        <td class="lotDescription">La messe du soir</td>
-                        <td class="lotMedium">Huile sur panneau</td>
-                        <td class="lotLivraison">oui</td>
-                    </tr>
-                </tbody>
-            </table>
+        <div class="d-flex">
+            <div class="table-responsive d-flex-1 d-flex flex-column">
+                <table class="table table-striped flex-grow-1 w-100">
+                    <colgroup>
+                        <col span="1" id="lotIdColonne"> <!--style="visibility:collapse;"-->
+                        <col span="1" id="lotEncanColonne">
+                        <col span="1" id="lotNumeroColonne">
+                        <col span="1" id="lotPrixOuvertureColonne">
+                        <col span="1" id="lotValeurMinPourVendreColonne">
+                        <col span="1" id="lotEstimationMinColonne">
+                        <col span="1" id="lotEstimationMaxColonne">
+                        <col span="1" id="lotCategorieColonne">
+                        <col span="1" id="lotArtisteColonne">
+                        <col span="1" id="lotDimensionColonne">
+                        <col span="1" id="lotDescriptionColonne">
+                        <col span="1" id="lotMediumColonne">
+                        <col span="1" id="lotLivraisonColonne">
+                    </colgroup>
+                    <thead>
+                        <tr>
+                            <th class="lotId">Lot Id</th>
+                            <th class="lotEncan">Encan</th>
+                            <th class="lotNumero">Lot #</th>
+                            <th class="lotPrixOuverture">Prix Ouverture</th>
+                            <th class="lotValeurMinPourVendre">Valeur Min Pour Vente</th>
+                            <th class="lotEstimationMin">Estimation Min</th>
+                            <th class="lotEstimationMax">Estimation Max</th>
+                            <th class="lotCategorie">Categorie</th>
+                            <th class="lotArtiste">Artiste</th>
+                            <th class="lotDimension">Dimension (en po)</th>
+                            <th class="lotDescription">Description</th>
+                            <th class="lotMedium">Medium</th>
+                            <th class="lotLivraison">Livraison</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="lotId">1</td>
+                            <td class="lotEncan">232</td>
+                            <td class="lotNumero">1</td>
+                            <td class="lotPrixOuverture">200$</td>
+                            <td class="lotValeurMinPourVendre"></td>
+                            <td class="lotEstimationMin">600$</td>
+                            <td class="lotEstimationMax">800$</td>
+                            <td class="lotCategorie">Art-Paintings</td>
+                            <td class="lotArtiste">Guy Paquet</td>
+                            <td class="lotDimension">12 x 16</td>
+                            <td class="lotDescription">1984/La nouvelle pratique</td>
+                            <td class="lotMedium">Huile sur panneau</td>
+                            <td class="lotLivraison">oui</td>
+                        </tr>
+                        <tr>
+                            <td class="lotId">2</td>
+                            <td class="lotEncan">232</td>
+                            <td class="lotNumero">1a</td>
+                            <td class="lotPrixOuverture">2200$</td>
+                            <td class="lotValeurMinPourVendre">3800$</td>
+                            <td class="lotEstimationMin">3500$</td>
+                            <td class="lotEstimationMax">5000$</td>
+                            <td class="lotCategorie">Art-Paintings</td>
+                            <td class="lotArtiste">Vladimir Horik</td>
+                            <td class="lotDimension">20 x 30</td>
+                            <td class="lotDescription">La messe du soir</td>
+                            <td class="lotMedium">Huile sur panneau</td>
+                            <td class="lotLivraison">oui</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="d-flex d-flex-1" style="background-color: red;">
+                <button class="btn d-flex-1 bleuMoyenFond btnSurvolerBleuMoyenFond text-white boutonOngletEncans" type="button">
+                    Encans
+                </button>
+                <div class="d-flex-1 bleuMoyenFond ongletEncans">
+
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -161,6 +171,8 @@
     table {
         border-collapse: collapse;
         margin-bottom: 50px;
+        font-size: 15px;
+        text-align: center;
     }
 
     table, th, td {
@@ -168,8 +180,28 @@
     }
 
     th, td {
-        display: none;
         padding: 5px;
+    }
+
+    th {
+        font-weight: bold;
+    }
+
+    col.cacherColonne {
+        visibility: collapse;
+    }
+
+    .ongletEncans {
+        height: 200px;
+        width: 100px;
+    }
+
+    .boutonOngletEncans {
+        border-top-left-radius: 50px;
+        border-top-right-radius: 50px;
+        height: 40px;
+        width: 180px;
+        transform: rotate(-90deg);
     }
 </style>
 
@@ -177,27 +209,68 @@
     import { onMounted } from 'vue';
 
     onMounted(() => {
-        document.querySelector("#lotIdCheckbox").addEventListener("change", basculerLaPresentationDeLaColonneLotId)
+        document.querySelector("#lotIdCheckbox")
+            .addEventListener("change", function () {
+                document.querySelector("#lotIdColonne").classList.toggle("cacherColonne");
+            })
+        document.querySelector("#lotEncanCheckbox")
+            .addEventListener("change", function () {
+                document.querySelector("#lotEncanColonne").classList.toggle("cacherColonne");
+            })
+        document.querySelector("#lotNumeroCheckbox")
+            .addEventListener("change", function () {
+                document.querySelector("#lotNumeroColonne").classList.toggle("cacherColonne");
+            })
+        document.querySelector("#lotPrixOuvertureCheckbox")
+            .addEventListener("change", function () {
+                document.querySelector("#lotPrixOuvertureColonne").classList.toggle("cacherColonne");
+            })
+        document.querySelector("#lotValeurMinPourVendreCheckbox")
+            .addEventListener("change", function () {
+                document.querySelector("#lotValeurMinPourVendreColonne").classList.toggle("cacherColonne");
+            })
+        document.querySelector("#lotEstimationMinCheckbox")
+            .addEventListener("change", function () {
+                document.querySelector("#lotEstimationMinColonne").classList.toggle("cacherColonne");
+            })
+        document.querySelector("#lotEstimationMaxCheckbox")
+            .addEventListener("change", function () {
+                document.querySelector("#lotEstimationMaxColonne").classList.toggle("cacherColonne");
+            })
+        document.querySelector("#lotCategorieCheckbox")
+            .addEventListener("change", function () {
+                document.querySelector("#lotCategorieColonne").classList.toggle("cacherColonne");
+            })
+        document.querySelector("#lotArtisteCheckbox")
+            .addEventListener("change", function () {
+                document.querySelector("#lotArtisteColonne").classList.toggle("cacherColonne");
+            })
+        document.querySelector("#lotDimensionCheckbox")
+            .addEventListener("change", function () {
+                document.querySelector("#lotDimensionColonne").classList.toggle("cacherColonne");
+            })
+        document.querySelector("#lotDescriptionCheckbox")
+            .addEventListener("change", function () {
+                document.querySelector("#lotDescriptionColonne").classList.toggle("cacherColonne");
+            })
+        document.querySelector("#lotMediumCheckbox")
+            .addEventListener("change", function () {
+                document.querySelector("#lotMediumColonne").classList.toggle("cacherColonne");
+            })
+        document.querySelector("#lotLivraisonCheckbox")
+            .addEventListener("change", function () {
+                document.querySelector("#lotLivraisonColonne").classList.toggle("cacherColonne");
+            })
     })
 
-    function basculerLaPresentationDeLaColonneLotId() {
-        if (this.isChecked) {
-            document.querySelector("#lotIdColonne").style.visibility = "visible";
-        }
-        else { document.querySelector("#lotIdColonne").style.visibility = "collapse"; }
-    }
+    //const listeDesCheckboxes = document.querySelector("input");
+    //const listeDesColonnes = document.querySelector("col");
 
-    //const listeDesCheckboxes = document.getElementByType("checkbox");
-    //const listeDesColonnes = document.getElementByType("col");
-
-    //function attribuerCheckboxesAuxColonnes(itemCheckbox, index) {
-    //document.querySelector("#lotIdCheckbox").addEventListener("change", () => {
-    //    if (this.isChecked) {
-    //        document.querySelector("#lotIdColonne").style.visibility = "visible";
-    //    }
-    //    else { document.querySelector("#lotIdColonne").style.visibility = "collapse"; }
-    //})
+    //listeDesCheckboxes.forEach(attribuerLesCheckboxesAuxColonnes);
+    //function attribuerLesCheckboxesAuxColonnes(item, index) {
+    //    item.addEventListener("change", function () { basculerLaPresentationDeLaColonne(index) })
     //}
-
-    //listeDesCheckboxes.forEach(attribuerCheckboxesAuxColonnes);
+    //function basculerLaPresentationDeLaColonne(index) {
+    //    listeDesColonnes[index].classList.toggle("cacherColonne");
+    //}
 </script>
