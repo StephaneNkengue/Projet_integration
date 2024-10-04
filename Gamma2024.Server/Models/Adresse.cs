38ffb15cd8e17ff1605b902c96518b9d011f2e0e
AdaNeil <2135621@CegepGranby.qc.ca>
2024-10-04 13:56:12 -0400

    Modification de la table Adresse afin de resoudre des problemes lors de l'ajout de Vendeurs

--- Gamma2024.Server/Models/Adresse.cs
+++ Gamma2024.Server/Models/Adresse.cs
@@ -10,16 +10,14 @@ namespace Gamma2024.Server.Models
         public string? Appartement { get; set; }
         public string Rue { get; set; } = null!;
         public string Ville { get; set; } = null!;
-        public string Province { get; set; } = null!;   
+        public string Province { get; set; } = null!;
         public string Pays { get; set; } = null!;
         [StringLength(6)]
         public string CodePostal { get; set; } = null!;
         public bool EstDomicile { get; set; }
         public string? IdApplicationUser { get; set; }
-        public int? IdVendeur { get; set; }
 
         public ApplicationUser? ApplicationUser { get; set; }
-        public Vendeur? Vendeur { get; set; }
         public ICollection<Facture> Factures { get; set; } = [];
     }
 }
