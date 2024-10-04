9e8710f953713430585080a1aea15037f0db591f
AdaNeil <2135621@CegepGranby.qc.ca>
2024-10-03 22:30:16 -0400

    Ajout du code pour convertir les lignes dans le csv en Vendeur dans l'extension

--- Gamma2024.Server/Extensions/VendeurExtensions.cs
+++ Gamma2024.Server/Extensions/VendeurExtensions.cs
@@ -1,6 +1,36 @@
+using Gamma2024.Server.Models;
+
 namespace Gamma2024.Server.Extensions
 {
     public static class VendeurExtensions
     {
+        public static IEnumerable<Vendeur> ToVendeur(this IEnumerable<string> source)
+        {
+            foreach (var line in source)
+            {
+                var columns = line.Split(';');
+
+                var adresse = columns[2].Split(",");
+                var numCiviqueRue = adresse[0].Split(" ", 1);
+
+                yield return new Vendeur
+                {
+                    Nom = columns[0],
+                    Prenom = columns[1],
+                    Adresse = new Adresse
+                    {
+                        Numero = int.Parse(numCiviqueRue[0]),
+                        Rue = numCiviqueRue[1],
+                        Ville = adresse[1],
+                        Province = adresse[2],
+                        Pays = "Canada",
+                        CodePostal = adresse[3],
+                        EstDomicile = false
+                    },
+                    Telephone = columns[3],
+                    Courriel = columns[4]
+                };
+            }
+        }
     }
 }
--- Gamma2024.Server/Models/Vendeur.cs
+++ Gamma2024.Server/Models/Vendeur.cs
@@ -1,6 +1,6 @@
 namespace Gamma2024.Server.Models
 {
-        public class Vendeur
+    public class Vendeur
     {
         public int Id { get; set; }
         public string Nom { get; set; } = null!;
@@ -8,7 +8,9 @@ namespace Gamma2024.Server.Models
         public string Courriel { get; set; } = null!;
         public string Telephone { get; set; } = null!;
         public int IdAdresse { get; set; }
-        
-        public  ICollection<Lot> Lots { get; set; } = [];
+
+        public Adresse Adresse { get; set; }
+
+        public ICollection<Lot> Lots { get; set; } = [];
     }
 }
