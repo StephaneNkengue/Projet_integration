3abe896d148e00c4dc8b5ebf2e307945789ae6e8
AdaNeil <2135621@CegepGranby.qc.ca>
2024-10-05 12:31:40 -0400

    Changement de la methode GetCategories afin de sauter des lignes vides (donc qui n'ont pas un numero de lot)

--- Gamma2024.Server/Extensions/Encans232et233CSVExtensions.cs
+++ Gamma2024.Server/Extensions/Encans232et233CSVExtensions.cs
@@ -1,7 +1,24 @@
+using Gamma2024.Server.Models;
+
 namespace Gamma2024.Server.Extensions
 {
     public static class Encans232et233CSVExtensions
     {
+        public static IEnumerable<Categorie> GetCategories(this IEnumerable<string> source)
+        {
+            foreach (var line in source)
+            {
+                var columns = line.Split(';');
 
+                if (!string.IsNullOrEmpty(columns[1]))
+                {
+                    yield return new Categorie
+                    {
+                        Nom = columns[6]
+                    };
+                }
+
+            }
+        }
     }
 }
