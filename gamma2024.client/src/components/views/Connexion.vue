643c52e6b8f8bba86579c150621591287d5394a7
StephaneNkengue <nkenguestephane0@gmail.com>
2024-10-06 10:05:22 -0400

    Ajout des spinner lors de l'inscription et la connexion

--- gamma2024.client/src/components/views/Connexion.vue
+++ gamma2024.client/src/components/views/Connexion.vue
@@ -1,142 +1,181 @@
 <template>
-    <div class="bg-image pt-5 imageDeFondEsquise">
-        <div class="container d-flex flex-column justify-content-start align-items-stretch bg-white bg-opacity-75 cadreBlanc">
-            <h2 class="fs-1 text-center fw-bold pt-4">Connexion</h2>
-            <p class="text-center">Se connecter pour continuer</p>
+  <div class="bg-image pt-5 imageDeFondEsquise">
+    <div
+      class="container d-flex flex-column justify-content-start align-items-stretch bg-white bg-opacity-75 cadreBlanc"
+    >
+      <h2 class="fs-1 text-center fw-bold pt-4">Connexion</h2>
+      <p class="text-center">Se connecter pour continuer</p>
 
-            <form @submit.prevent="connexion" class="d-flex flex-column justify-content-start align-items-stretch">
-                <div class="px-4 py-2 mb-4">
-                    <div class="d-flex flex-row justify-content-center mb-3">
-                        <div class="form-group w-90">
-                            <label for="emailOuPseudo" class="fw-bold ms-3">Email ou pseudonyme</label>
-                            <input type="text"
-                                   class="form-control"
-                                   id="emailOuPseudo"
-                                   v-model="emailOuPseudo"
-                                   @input="validateEmailOuPseudo"
-                                   :class="{ 'is-invalid': emailOuPseudoError }" />
-                            <div v-if="emailOuPseudoError" class="invalid-feedback">{{ emailOuPseudoError }}</div>
-                        </div>
-                    </div>
-                    <div class="d-flex flex-row justify-content-center mb-3">
-                        <div class="form-group w-90">
-                            <label for="motdepasse" class="fw-bold ms-3">Mot de passe</label>
-                            <input type="password"
-                                   class="form-control"
-                                   id="motDePasse"
-                                   v-model="password"
-                                   @input="validatePassword"
-                                   :class="{ 'is-invalid': passwordError }" />
-                            <div v-if="passwordError" class="invalid-feedback">{{ passwordError }}</div>
-                        </div>
-                    </div>
-                    <div class="d-flex flex-row justify-content-center mt-4 mb-1">
-                        <div class="form-group w-80">
-                            <router-link to="/" class="text-decoration-none">
-                                <p class="text-center lienEnTexteNoir">J'ai oublié mon Mot de passe</p>
-                            </router-link>
-                        </div>
-                    </div>
-                    <div class="d-flex flex-row justify-content-center mb-2">
-                            <button class="btn btnSurvolerBleuMoyenFond rounded-pill px-5 bleuMoyenFond text-white" @click="connexion">Connexion</button>
-                    </div>
-                    <div class="d-flex flex-row justify-content-center mb-3">
-                        <div class="form-group w-80">
-                            <router-link to="Inscription" class="text-decoration-none">
-                                <p class="fs-5 text-center fw-bold lienEnTexteNoir">M'inscrire</p>
-                            </router-link>
-                        </div>
-                    </div>
-
-                </div>
-            </form>
-
+      <form
+        @submit.prevent="connexion"
+        class="d-flex flex-column justify-content-start align-items-stretch"
+      >
+        <div class="px-4 py-2 mb-4">
+          <div class="d-flex flex-row justify-content-center mb-3">
+            <div class="form-group w-90">
+              <label for="emailOuPseudo" class="fw-bold ms-3"
+                >Email ou pseudonyme</label
+              >
+              <input
+                type="text"
+                class="form-control"
+                id="emailOuPseudo"
+                v-model="emailOuPseudo"
+                @input="validateEmailOuPseudo"
+                :class="{ 'is-invalid': emailOuPseudoError }"
+              />
+              <div v-if="emailOuPseudoError" class="invalid-feedback">
+                {{ emailOuPseudoError }}
+              </div>
+            </div>
+          </div>
+          <div class="d-flex flex-row justify-content-center mb-3">
+            <div class="form-group w-90">
+              <label for="motdepasse" class="fw-bold ms-3">Mot de passe</label>
+              <input
+                type="password"
+                class="form-control"
+                id="motDePasse"
+                v-model="password"
+                @input="validatePassword"
+                :class="{ 'is-invalid': passwordError }"
+              />
+              <div v-if="passwordError" class="invalid-feedback">
+                {{ passwordError }}
+              </div>
+            </div>
+          </div>
+          <div class="d-flex flex-row justify-content-center mt-4 mb-1">
+            <div class="form-group w-80">
+              <router-link to="/" class="text-decoration-none">
+                <p class="text-center lienEnTexteNoir">
+                  J'ai oublié mon Mot de passe
+                </p>
+              </router-link>
+            </div>
+          </div>
+          <div class="d-flex flex-row justify-content-center mb-2">
+            <button
+              :disabled="isSubmitting"
+              class="btn btnSurvolerBleuMoyenFond rounded-pill px-5 bleuMoyenFond text-white"
+              @click="connexion"
+            >
+              <span
+                v-if="isSubmitting"
+                class="spinner-grow spinner-grow-sm"
+                role="status"
+                aria-hidden="true"
+              ></span>
+              Connexion
+            </button>
+          </div>
+          <div class="d-flex flex-row justify-content-center mb-3">
+            <div class="form-group w-80">
+              <router-link to="Inscription" class="text-decoration-none">
+                <p class="fs-5 text-center fw-bold lienEnTexteNoir">
+                  M'inscrire
+                </p>
+              </router-link>
+            </div>
+          </div>
         </div>
+      </form>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                emailOuPseudo: '',
-                password: '',
-                emailOuPseudoError: '',
-                passwordError: '',
-                messageErreur: '',
-                messageSucces: ''
-            }
-        },
-        methods: {
-            validateEmailOuPseudo() {
-                // Validation logic here
-            },
-            validatePassword() {
-                // Validation logic here
-            },
-            async connexion() {
-                this.validateEmailOuPseudo();
-                this.validatePassword();
+export default {
+  data() {
+    return {
+      emailOuPseudo: "",
+      password: "",
+      emailOuPseudoError: "",
+      passwordError: "",
+      messageErreur: "",
+      messageSucces: "",
+      isSubmitting: false,
+    };
+  },
+  methods: {
+    validateEmailOuPseudo() {
+      // Validation logic here
+    },
+    validatePassword() {
+      // Validation logic here
+    },
+    async connexion() {
+      this.validateEmailOuPseudo();
+      this.validatePassword();
+      this.isSubmitting = true;
 
-                if (this.emailOuPseudoError || this.passwordError) {
-                    return; // Empêche la soumission si des erreurs sont présentes
-                }
+      if (this.emailOuPseudoError || this.passwordError) {
+        this.isSubmitting = false;
+        return; // Empêche la soumission si des erreurs sont présentes
+      }
 
-                try {
-                    console.log('Tentative de connexion avec:', { emailOuPseudo: this.emailOuPseudo, password: this.password });
-                    const result = await this.$store.dispatch('login', {
-                        email: this.emailOuPseudo,
-                        password: this.password
-                    });
-                    if (result.success) {
-                        this.messageSucces = `Connexion réussie en tant que ${result.roles.join(', ')}`;
-                        console.log('Utilisateur connecté:', this.$store.state.user);
-                        console.log('Rôles:', this.$store.state.roles);
-                        // Redirection immédiate vers la page d'accueil
-                        this.$router.push('/');
-                    } else {
-                        this.messageErreur = 'Échec de la connexion: ' + result.error;
-                    }
-                } catch (error) {
-                    console.error("Erreur détaillée lors de la connexion:", error);
-                    this.messageErreur = "Erreur lors de la connexion: " + error;
-                }
-            }
+      try {
+        console.log("Tentative de connexion avec:", {
+          emailOuPseudo: this.emailOuPseudo,
+          password: this.password,
+        });
+        const result = await this.$store.dispatch("login", {
+          email: this.emailOuPseudo,
+          password: this.password,
+        });
+        if (result.success) {
+          this.messageSucces = `Connexion réussie en tant que ${result.roles.join(
+            ", "
+          )}`;
+          console.log("Utilisateur connecté:", this.$store.state.user);
+          console.log("Rôles:", this.$store.state.roles);
+          // Redirection immédiate vers la page d'accueil
+          this.$router.push("/");
+        } else {
+          this.messageErreur = "Échec de la connexion: " + result.error;
         }
-    }
+      } catch (error) {
+        console.error("Erreur détaillée lors de la connexion:", error);
+        this.messageErreur = "Erreur lors de la connexion: " + error;
+      } finally {
+        this.isSubmitting = false;
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
-    .cadreBlanc {
-        border-radius: 15px;
-        min-height: 450px;
-        width: 410px;
-    }
+.cadreBlanc {
+  border-radius: 15px;
+  min-height: 450px;
+  width: 410px;
+}
 
-    .imageDeFondEsquise {
-        background-image: url('/public/images/DessinGris.PNG');
-        background-size: cover;
-        background-position: 0px -100px;
-        background-attachment: fixed;
-        height: 100vh;
-        width: 100%;
-    }
+.imageDeFondEsquise {
+  background-image: url("/public/images/DessinGris.PNG");
+  background-size: cover;
+  background-position: 0px -100px;
+  background-attachment: fixed;
+  height: 100vh;
+  width: 100%;
+}
 
-    .w-80 {
-        width: 80%;
-    }
+.w-80 {
+  width: 80%;
+}
 
-    .w-90 {
-        width: 90%;
-    }
+.w-90 {
+  width: 90%;
+}
 
-    .text-danger {
-        font-size: 0.875rem;
-    }
+.text-danger {
+  font-size: 0.875rem;
+}
 
-    .invalid-feedback {
-        display: block;
-        color: #dc3545;
-        font-size: 0.875rem;
-    }
+.invalid-feedback {
+  display: block;
+  color: #dc3545;
+  font-size: 0.875rem;
+}
 </style>
