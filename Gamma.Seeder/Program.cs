0193c0e4a65af81a98d1cd14a5a2d3811355ec39
AdaNeil <2135621@CegepGranby.qc.ca>
2024-10-07 10:37:14 -0400

    Ajout du code pour appeller les nouvelles methodes

--- Gamma.Seeder/Program.cs
+++ Gamma.Seeder/Program.cs
@@ -6,15 +6,29 @@ var context = DbContextFactory.CreateDbContext();
 Console.WriteLine("Début du seed...");
 
 
+Console.WriteLine("Ajout des vendeurs");
 var vendeurs = File.ReadAllLines("CSV/Vendeurs.csv", System.Text.Encoding.GetEncoding("iso-8859-1"))
                 .Skip(1)
                 .Where(l => l.Length > 1)
                 .ToVendeur()
                 .ToList();
-
 context.Vendeurs.AddRange(vendeurs);
 context.SaveChanges();
 
+var lotsVendeurs232 = File.ReadAllLines("CSV/Vendeurs.csv", System.Text.Encoding.GetEncoding("iso-8859-1"))
+                .Skip(1)
+                .Where(l => l.Length > 1)
+                .GetNumeroLotsEncan232()
+                .ToList();
+
+var lotsVendeurs233 = File.ReadAllLines("CSV/Vendeurs.csv", System.Text.Encoding.GetEncoding("iso-8859-1"))
+                .Skip(1)
+                .Where(l => l.Length > 1)
+                .GetNumeroLotsEncan233()
+                .ToList();
+
+
+Console.WriteLine("Ajout des categories");
 var categoriesLotsUniques = File.ReadAllLines("CSV/Encan232et233.csv", System.Text.Encoding.GetEncoding("iso-8859-1"))
                         .Skip(1)
                         .Where(l => l.Length > 1)
@@ -22,10 +36,10 @@ var categoriesLotsUniques = File.ReadAllLines("CSV/Encan232et233.csv", System.Te
                         .GroupBy(c => c.Nom)
                         .Select(c => c.First())
                         .ToList();
-
 context.Categories.AddRange(categoriesLotsUniques);
 context.SaveChanges();
 
+Console.WriteLine("Ajout des médiums");
 var mediumsLotsUniques = File.ReadAllLines("CSV/Encan232et233.csv", System.Text.Encoding.GetEncoding("iso-8859-1"))
                         .Skip(1)
                         .Where(l => l.Length > 1)
@@ -33,14 +47,14 @@ var mediumsLotsUniques = File.ReadAllLines("CSV/Encan232et233.csv", System.Text.
                         .GroupBy(m => m.Type)
                         .Select(m => m.First())
                         .ToList();
-
 context.Mediums.AddRange(mediumsLotsUniques);
 context.SaveChanges();
 
-var lots = File.ReadAllLines("CSV/Encan232et233.csv", System.Text.Encoding.GetEncoding("iso-8859-1"))
+
+var lots232 = File.ReadAllLines("CSV/Encan232et233.csv", System.Text.Encoding.GetEncoding("iso-8859-1"))
                         .Skip(1)
                         .Where(l => l.Length > 1)
-                        .ToLot()
+                        .ToLotEncan232()
                         .Select(l =>
                         {
                             foreach (var item in categoriesLotsUniques)
@@ -61,13 +75,21 @@ var lots = File.ReadAllLines("CSV/Encan232et233.csv", System.Text.Encoding.GetEn
                                     break;
                                 }
                             }
-                            //temporairement
-                            l.IdVendeur = 1;
+                            var position = 0;
+                            foreach (var item in lotsVendeurs232)
+                            {
+                                if (item.Contains(l.Numero))
+                                {
+                                    l.IdVendeur = vendeurs[position].Id;
+                                    l.Vendeur = vendeurs[position];
+                                }
+                                position++;
+                            }
                             return l;
                         })
                         .ToList();
 
-context.Lots.AddRange(lots);
+context.Lots.AddRange(lots232);
 context.SaveChanges();
 
 Console.WriteLine("Fin du seed");
