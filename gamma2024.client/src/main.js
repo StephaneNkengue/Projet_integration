b5be0678bc7471b492cbb1000e2a92f0f9393956
StephaneNkengue <nkenguestephane0@gmail.com>
2024-10-08 12:34:06 -0400

    Merge de dev vers ma branche

--- gamma2024.client/src/main.js
+++ gamma2024.client/src/main.js
@@ -1,19 +1,21 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
-import router from './router'
-import PrimeVue from 'primevue/config'
-import Toast from 'vue3-toastify'
-import 'vue3-toastify/dist/index.css';
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store";
+import router from "./router";
+import PrimeVue from "primevue/config";
+import Toast from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
+import DataTable from "datatables.net-vue3";
+import DataTablesCore from "datatables.net-dt";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(store)
-    .use(Toast)
-    .use(router)
-    .use(PrimeVue)
-    .mount('#app');
-
-
+app
+  .use(store)
+  .use(DataTable)
+  .use(Toast)
+  .use(router)
+  .use(PrimeVue)
+  .mount("#app");
