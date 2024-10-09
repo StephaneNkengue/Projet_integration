49c1adfa274a27b4befa2283f012da30e84260b3
AdaNeil <2135621@CegepGranby.qc.ca>
2024-10-09 15:39:12 -0400

    Modification de l'extension pour créer les utilisateurs afin d'aller chercher le bon avatar

--- Gamma2024.Server/Extensions/AcheteursCSVExtensions.cs
+++ Gamma2024.Server/Extensions/AcheteursCSVExtensions.cs
@@ -36,7 +36,7 @@ namespace Gamma2024.Server.Extensions
                     Email = columns[5],
                     NormalizedEmail = columns[5].ToUpper(),
                     EmailConfirmed = true,
-                    Avatar = "/Gamma2024.Server/Avatars/default.png",
+                    Avatar = "default.png",
                     SecurityStamp = Guid.NewGuid().ToString(),
                 };
 
