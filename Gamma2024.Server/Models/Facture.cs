33c27373e9bc02b0d7a99b0e22af55c151dd692b
AdaNeil <2135621@CegepGranby.qc.ca>
2024-10-15 12:05:30 -0400

    Ajout d'attributs importants dans facture
    Ajout des attributs pour les differents frais dans la facture

--- Gamma2024.Server/Models/Facture.cs
+++ Gamma2024.Server/Models/Facture.cs
@@ -1,19 +1,21 @@
-namespace Gamma2024.Server.Models{
-    public class Facture
+namespace Gamma2024.Server.Models
 {
-    public int Id { get; set; }
-    public int NumeroFacture { get; set; }
-    public DateTime DateAchat { get; set; }
-    public double PrixTotal { get; set; }
-    public bool EstLivree { get; set; }
-    public int IdLot { get; set; }
-    public int IdAdresse { get; set; }
-    public string IdClient { get; set; } = null!;
-    
-    public  Lot Lot { get; set; } = null!;
-    public  Adresse Adresse { get; set; } = null!;
-    public Charite? Charite { get; set; }
-    public ApplicationUser Client { get; set; } = null!;
-
-}
+    public class Facture
+    {
+        public int Id { get; set; }
+        public DateTime DateAchat { get; set; }
+        public double PrixFinal { get; set; }
+        public double PrixLots { get; set; }
+        public double FraisEncanteur { get; set; }
+        public double? FraisLivraison { get; set; }
+        public double Taxes { get; set; }
+        public double? Don { get; set; }
+        public int IdAdresse { get; set; }
+        public string IdClient { get; set; } = null!;
+        public int? IdCharite { get; set; }
+        public ICollection<Lot> Lots { get; set; } = null!;
+        public Adresse Adresse { get; set; } = null!;
+        public Charite? Charite { get; set; }
+        public ApplicationUser Client { get; set; } = null!;
+    }
 }
