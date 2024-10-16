86f263f693681060d336eb56523880ef712cf4c3
AdaNeil <2135621@CegepGranby.qc.ca>
2024-10-16 15:29:45 -0400

    Ajout d'un méthode dans facture pour calculer les frais de livraison

--- Gamma2024.Server/Models/Facture.cs
+++ Gamma2024.Server/Models/Facture.cs
@@ -13,9 +13,32 @@ namespace Gamma2024.Server.Models
         public int IdAdresse { get; set; }
         public string IdClient { get; set; } = null!;
         public int? IdCharite { get; set; }
-        public ICollection<Lot> Lots { get; set; } = null!;
+        public ICollection<Lot> Lots { get; set; } = [];
         public Adresse Adresse { get; set; } = null!;
         public Charite? Charite { get; set; }
         public ApplicationUser Client { get; set; } = null!;
+
+        public void CalculerFraisLivraison()
+        {
+            var prix = new List<double>();
+            foreach (var item in Lots)
+            {
+                if (item.Hauteur <= 20 || item.Largeur <= 20)
+                {
+                    prix.Add(35);
+                }
+                else
+                {
+                    prix.Add(65);
+                }
+            }
+            prix.OrderByDescending(p => p);
+
+            FraisLivraison = prix[0];
+
+            prix.RemoveAt(0);
+
+            FraisLivraison += (prix.Sum()) / 2;
+        }
     }
 }
