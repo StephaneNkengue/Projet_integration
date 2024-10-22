69ff7d10f18f0c7d18c0adea635612902689072f
AdaNeil <2135621@CegepGranby.qc.ca>
2024-10-22 12:41:54 -0400

    Ajout du fichier Encan.vue

---
+++ gamma2024.client/src/components/views/Encan.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="d-flex flex-column align-items-center pb-3">
+        <h1>Encan {{numeroEncan}}</h1>
+        <p>Date de debut de la soirée de cloture</p>
+
+        <AffichageLots />
+
+    </div>
+</template>
+
+<script setup>
+    import AffichageLots from '@/components/views/AffichageLots.vue'
+
+    defineProps({
+        numeroEncan: Number
+    })
+</script>
+
+<style scoped></style>
