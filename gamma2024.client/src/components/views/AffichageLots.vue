fd7764d43f9ec2d88a22e277b6dbaf630bf8f562
AdaNeil <2135621@CegepGranby.qc.ca>
2024-10-24 08:33:15 -0400

    Ajout d'un key dans le v-for pour les lots dans AffichageLots.vue afin de changer les lots qui sont affichés sur chaque page

--- gamma2024.client/src/components/views/AffichageLots.vue
+++ gamma2024.client/src/components/views/AffichageLots.vue
@@ -37,7 +37,7 @@
 
     <div v-if="siTuile"
          class="row row-cols-lg-5 row-cols-md-3 row-cols-sm-2 row-cols-1 w-100 px-3">
-        <div v-for="index in lotsAffiche" class="col p-2">
+        <div v-for="index in lotsAffiche" :key="index.id" class="col p-2">
             <LotTuile :lotRecu="index" />
         </div>
     </div>
