29eb41401d51572acfa3861c2deb3f8abdb89b2b
StephaneNkengue <nkenguestephane0@gmail.com>
2024-10-28 23:44:28 -0400

    Set environmenet variable

--- gamma2024.client/src/services/api.js
+++ gamma2024.client/src/services/api.js
@@ -1,30 +1,30 @@
 import axios from "axios";
 
 export const initApi = (getToken) => {
-    const api = axios.create({
-        baseURL: import.meta.env.VITE_API_URL,
-        withCredentials: true,
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-    });
+  const api = axios.create({
+    baseURL: `${import.meta.env.VITE_API_URL}${import.meta.env.VITE_BASE_URL}`,
+    withCredentials: true,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
 
-    // Intercepteur pour les requêtes
-    api.interceptors.request.use(
-        (config) => {
-            const token = getToken();
-            if (token) {
-                config.headers.Authorization = `Bearer ${token}`;
-            }
-            return config;
-        },
-        (error) => {
-            return Promise.reject(error);
-        }
-    );
+  // Intercepteur pour les requêtes
+  api.interceptors.request.use(
+    (config) => {
+      const token = getToken();
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
 
-    return api;
+  return api;
 };
 
 export default { initApi };
