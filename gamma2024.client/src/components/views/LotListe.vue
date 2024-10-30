1dc165a78a8db582ac2b87adbde532d8c18d45ac
StephaneNkengue <nkenguestephane0@gmail.com>
2024-10-30 00:10:06 -0400

    Final

--- gamma2024.client/src/components/views/LotListe.vue
+++ gamma2024.client/src/components/views/LotListe.vue
@@ -1,89 +1,112 @@
 <template>
-    <router-link class="text-decoration-none" :to="{name: 'DetailsLot', params: {idLot: lot.id}}">
-        <div class="card">
-            <div class="card-body">
-                <div class="d-flex flex-md-row flex-column flex-wrap justify-content-between gap-1">
-                    <div class="d-flex flex-column justify-content-center align-items-center">
-                        <img v-bind:src="urlApi + lot.photos[0].lien"
-                             height="150"
-                             alt="Image du lot" />
-                    </div>
+  <router-link
+    class="text-decoration-none"
+    :to="{ name: 'DetailsLot', params: { idLot: lot.id } }"
+  >
+    <div class="card">
+      <div class="card-body">
+        <div
+          class="d-flex flex-md-row flex-column flex-wrap justify-content-between gap-1"
+        >
+          <div
+            class="d-flex flex-column justify-content-center align-items-center"
+          >
+            <img
+              v-bind:src="urlApi + lot.photos[0].lien"
+              height="150"
+              alt="Image du lot"
+            />
+          </div>
 
+          <div
+            class="d-flex align-items-center justify-content-center gap-3 flex-md-column flex-row flex-lg-row"
+          >
+            <p class="mb-0">Lot {{ lot.numero }}</p>
+            <p class="mb-0">{{ lot.artiste }}</p>
+            <p class="mb-0">{{ lot.hauteur }} x {{ lot.largeur }} po</p>
+          </div>
 
-                    <div class="d-flex align-items-center justify-content-center gap-3 flex-md-column flex-row flex-lg-row">
-                        <p class=" mb-0">Lot {{lot.numero}}</p>
-                        <p class="mb-0">{{lot.artiste}}</p>
-                        <p class="mb-0">{{lot.hauteur}} x {{lot.largeur}} po</p>
-                    </div>
+          <div
+            class="d-flex flex-column flex-lg-row align-items-center gap-3 justify-content-center"
+          >
+            <p class="text-center mb-0">
+              Valeur: {{ lot.valeurEstimeMin.toFixed(0) }}$ -
+              {{ lot.valeurEstimeMax.toFixed(0) }}$
+            </p>
+            <p class="text-center mb-0">
+              Mise actuelle: {{ lot.mise.toFixed(0) }}$
+            </p>
+          </div>
 
+          <div
+            class="d-flex align-self-center gap-1 flex-column flex-md-row align-items-center"
+          >
+            <button
+              type="button"
+              class="btn bleuMoyenFond text-white btnSurvolerBleuMoyenFond justify-self-end m-1"
+            >
+              Miser
+            </button>
+            <img
+              src="/icons/IconeLivrable.png"
+              height="50"
+              width="50"
+              alt="Livrable"
+              v-if="lot.estLivrable"
+            />
 
-
-                    <div class="d-flex flex-column flex-lg-row align-items-center gap-3 justify-content-center">
-                        <p class=" text-center mb-0">Valeur: {{(lot.valeurEstimeMin).toFixed(0)}}$ - {{(lot.valeurEstimeMax).toFixed(0)}}$</p>
-                        <p class=" text-center mb-0">Mise actuelle: {{(lot.mise).toFixed(0)}}$</p>
-                    </div>
-
-                    <div class="d-flex align-self-center gap-1 flex-column flex-md-row align-items-center">
-                        <button type="button" class="btn bleuMoyenFond text-white btnSurvolerBleuMoyenFond justify-self-end m-1">Miser</button>
-                        <img src="/icons/IconeLivrable.png"
-                             height="50"
-                             width="50"
-                             alt="Livrable"
-                             v-if="lot.estLivrable" />
-
-                        <img src="/icons/IconeNonLivrable.png"
-                             height="50"
-                             width="50"
-                             alt="Non livrable"
-                             v-else />
-                    </div>
-                </div>
-            </div>
+            <img
+              src="/icons/IconeNonLivrable.png"
+              height="50"
+              width="50"
+              alt="Non livrable"
+              v-else
+            />
+          </div>
         </div>
-
-    </router-link>
+      </div>
+    </div>
+  </router-link>
 </template>
 
 <script setup>
-    import { onMounted, ref } from 'vue'
-    import { useStore } from "vuex";
+import { onMounted, ref } from "vue";
+import { useStore } from "vuex";
 
-    const store = useStore();
+const store = useStore();
 
-    const props = defineProps({
-        lotRecu: Object
-    })
+const props = defineProps({
+  lotRecu: Object,
+});
 
-    const urlApi = ref("/api")
-    const lot = ref({
-        "id": 0,
-        "numero": "",
-        "description": "",
-        "valeurEstimeMin": 0,
-        "valeurEstimeMax": 0,
-        "artiste": " ",
-        "mise": 0,
-        "estVendu": true,
-        "dateFinVente": "",
-        "estLivrable": true,
-        "largeur": 0,
-        "hauteur": 0,
-        "photos": [
-            {
-                "id": 0,
-                "lien": "",
-                "idLot": 0,
-                "lot": null
-            }
-        ]
-    })
+const urlApi = ref("/api");
+const lot = ref({
+  id: 0,
+  numero: "",
+  description: "",
+  valeurEstimeMin: 0,
+  valeurEstimeMax: 0,
+  artiste: " ",
+  mise: 0,
+  estVendu: true,
+  dateFinVente: "",
+  estLivrable: true,
+  largeur: 0,
+  hauteur: 0,
+  photos: [
+    {
+      id: 0,
+      lien: "",
+      idLot: 0,
+      lot: null,
+    },
+  ],
+});
 
-    onMounted(async () => {
-        lot.value = props.lotRecu
-        urlApi.value = await store.state.api.defaults.baseURL.replace("\api", "")
-    })
-
+onMounted(async () => {
+  lot.value = props.lotRecu;
+  urlApi.value = await store.state.api.defaults.baseURL.replace("\api", "");
+});
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
--- gamma2024.client/src/components/views/LotTuile.vue
+++ gamma2024.client/src/components/views/LotTuile.vue
@@ -1,86 +1,106 @@
 <template>
-    <router-link class="text-decoration-none d-flex align-self-stretch" :to="{name: 'DetailsLot', params: {idLot: lot.id}}">
-        <div class="card align-self-stretch">
-            <div class="card-body d-flex flex-column justify-content-between">
-                <div class="d-flex align-items-middle justify-content-between">
-                    <p class="fs-7 pe-2 mt-1 mb-0">Lot {{lot.numero}} {{lot.artiste}} {{lot.hauteur}} x {{lot.largeur}} po</p>
+  <router-link
+    class="text-decoration-none card d-flex align-self-stretch"
+    :to="{ name: 'DetailsLot', params: { idLot: lot.id } }"
+  >
+    <div class="card align-self-stretch">
+      <div class="card-body d-flex flex-column justify-content-between">
+        <div class="d-flex align-items-middle justify-content-between">
+          <p class="fs-7 pe-2 mt-1 mb-0">
+            Lot {{ lot.numero }} {{ lot.artiste }} {{ lot.hauteur }} x
+            {{ lot.largeur }} po
+          </p>
 
-                    <img src="/icons/IconeLivrable.png"
-                         height="30"
-                         alt="Livrable"
-                         v-if="lot.estLivrable" />
+          <img
+            src="/icons/IconeLivrable.png"
+            height="30"
+            alt="Livrable"
+            v-if="lot.estLivrable"
+          />
 
-                    <img src="/icons/IconeNonLivrable.png"
-                         height="30"
-                         alt="Non livrable"
-                         v-else />
-                </div>
-
-                <div class="row justify-content-around align-items-middle">
-                    <img v-bind:src="urlApi + lot.photos[0].lien"
-                         class="img-fluid my-2 col-8 col-sm-10 col-md-12 "
-                         alt="Image du lot" />
-
-                </div>
-
-                <p class="text-center mb-0">Valeur: {{(lot.valeurEstimeMin).toFixed(0)}}$ - {{(lot.valeurEstimeMax).toFixed(0)}}$</p>
-                <p class="text-center mb-0">Mise actuelle: {{(lot.mise).toFixed(0)}}$</p>
-
-                <div class="d-flex justify-content-around pt-2">
-                    <button type="button" class="btn bleuMoyenFond text-white btnSurvolerBleuMoyenFond">Miser</button>
-                </div>
-
-            </div>
-
+          <img
+            src="/icons/IconeNonLivrable.png"
+            height="30"
+            alt="Non livrable"
+            v-else
+          />
         </div>
 
-    </router-link>
+        <div class="row justify-content-around align-items-middle">
+          <img
+            v-bind:src="urlApi + lot.photos[0].lien"
+            class="img-fluid my-2 col-8 col-sm-10 col-md-12"
+            alt="Image du lot"
+          />
+        </div>
 
+        <p class="text-center mb-0">
+          Valeur: {{ lot.valeurEstimeMin.toFixed(0) }}$ -
+          {{ lot.valeurEstimeMax.toFixed(0) }}$
+        </p>
+        <p class="text-center mb-0">
+          Mise actuelle: {{ lot.mise.toFixed(0) }}$
+        </p>
+
+        <div class="d-flex justify-content-around pt-2">
+          <button
+            type="button"
+            class="btn bleuMoyenFond text-white btnSurvolerBleuMoyenFond"
+          >
+            Miser
+          </button>
+        </div>
+      </div>
+    </div>
+  </router-link>
 </template>
 
 <script setup>
-    import { onMounted, ref } from 'vue'
-    import { useStore } from "vuex";
+import { onMounted, ref } from "vue";
+import { useStore } from "vuex";
 
-    const store = useStore();
+const store = useStore();
 
-    const props = defineProps({
-        lotRecu: Object
-    })
+const props = defineProps({
+  lotRecu: Object,
+});
 
-    const urlApi = ref("/api")
-    const lot = ref({
-        "id": 0,
-        "numero": "",
-        "description": "",
-        "valeurEstimeMin": 0,
-        "valeurEstimeMax": 0,
-        "artiste": " ",
-        "mise": 0,
-        "estVendu": true,
-        "dateFinVente": "",
-        "estLivrable": true,
-        "largeur": 0,
-        "hauteur": 0,
-        "photos": [
-            {
-                "id": 0,
-                "lien": "",
-                "idLot": 0,
-                "lot": null
-            }
-        ]
-    })
+const urlApi = ref("/api");
+const lot = ref({
+  id: 0,
+  numero: "",
+  description: "",
+  valeurEstimeMin: 0,
+  valeurEstimeMax: 0,
+  artiste: " ",
+  mise: 0,
+  estVendu: true,
+  dateFinVente: "",
+  estLivrable: true,
+  largeur: 0,
+  hauteur: 0,
+  photos: [
+    {
+      id: 0,
+      lien: "",
+      idLot: 0,
+      lot: null,
+    },
+  ],
+});
 
-    onMounted(async () => {
-        lot.value = props.lotRecu
-        urlApi.value = await store.state.api.defaults.baseURL.replace("\api", "")
-    })
-
+onMounted(async () => {
+  lot.value = props.lotRecu;
+  urlApi.value = await store.state.api.defaults.baseURL.replace("\api", "");
+});
 </script>
 
 <style scoped>
-    .fs-7 {
-        font-size: 0.89rem;
-    }
+.fs-7 {
+  font-size: 0.89rem;
+}
+
+.card {
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.2), 0 3px 5px 0 rgba(0, 0, 0, 0.19);
+}
 </style>
