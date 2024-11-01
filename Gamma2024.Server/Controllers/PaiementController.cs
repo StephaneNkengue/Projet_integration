29bee9c38e17d0d27d58ebd1520bb02839f7eff7
AdaNeil <2135621@CegepGranby.qc.ca>
2024-11-01 08:31:31 -0400

    Ajout d'un code temporaire permettant de chercher un facture en fonction de son id et le payer

--- Gamma2024.Server/Controllers/PaiementController.cs
+++ Gamma2024.Server/Controllers/PaiementController.cs
@@ -1,4 +1,4 @@
-using Gamma2024.Server.Models;
+using Gamma2024.Server.Data;
 using Microsoft.AspNetCore.Mvc;
 using Stripe;
 
@@ -8,15 +8,30 @@ namespace Gamma2024.Server.Controllers
     [Route("api/[controller]")]
     public class PaiementController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Payer(Facture facture)
+        private readonly ApplicationDbContext _context;
+
+        public PaiementController(ApplicationDbContext context)
         {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Payer(int idFacture)
+        {
+            var facture = _context.Factures.FirstOrDefault(f => f.Id == idFacture);
+
+            if (facture == null)
+            {
+                return NotFound();
+            }
+
+            var prix = (long)facture.PrixFinal * 100;
+
             var options = new ChargeCreateOptions
             {
-                Amount = 2,
+                Amount = prix,
                 Currency = "cad",
                 Source = "tok_amex",
-                Description = "Test Payer",
+                Description = "Test",
             };
             var service = new ChargeService();
             service.Create(options);
