d4418e408f13c5d87f78f8ef4d08cb5b046bdf0b
ThiMaiTrang <2162067@CegepGranby.qc.ca>
2024-10-31 22:27:03 -0400

    Ajout de quelques éléments visuels de base pour le TableauDeBordVentes

--- gamma2024.client/src/components/views/TableauDeBordVentes.vue
+++ gamma2024.client/src/components/views/TableauDeBordVentes.vue
@@ -1,8 +1,74 @@
 <template>
+    <div class="d-flex flex-column justify-content-between">
+        <div class="d-flex justify-content-between">
+            <h2 class="d-flex-1">Liste des ventes</h2>
+        </div>
+
+        <div class="d-flex flex-row-reverse me-1 gap-1 align-items-center">
+
+            <input data-bs-theme="light"
+                   type="search"
+                   aria-label="Recherche" /><label for="Recherche">Rechercher: </label>
+        </div>
+
+        <table class="table table-striped mt-5 mx-0 text-center">
+            <thead>
+                <tr>
+                    <th data-field="dateAchat">Date de l'achat</th>
+                    <th data-field="prixFinal">Prix final</th>
+                    <th data-field="fraisEncanteur">Frais d'encanteur</th>
+                    <th data-field="fraisLivraison">Frais de livraison</th>
+                    <th data-field="tps">TPS</th>
+                    <th data-field="tvq">TVQ</th>
+                    <th data-field="don">Don</th>
+                    <th data-field="client">Client</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td class="align-middle">
+                        <span>
+                            <button class="btn btn-info">
+                                <img src="/images/ice.png" class="img-fluid" alt="..." />
+                            </button>
+                        </span>
+
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script setup>
+
 </script>
 
 <style scoped>
+    img {
+        width: 25px;
+        height: 30px;
+    }
+
+    table,
+    input {
+        box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.2), 0 3px 5px 0 rgba(0, 0, 0, 0.19);
+    }
+
+    th {
+        font-size: 18px;
+    }
+
+    td {
+        font-size: 16px;
+    }
 </style>
