f77ab430d15bc3c16883ad6cf790d84535046f85
ThiMaiTrang <2162067@CegepGranby.qc.ca>
2024-11-04 10:53:24 -0500

    Ajout du premier tableau dans l'onglet Par encan dans la page TableauDeBord-Ventes
    Ajout de css nécessaire pour enlever et appliquer les styles de PrimeVue
    Installation du package pour le thème de PrimeVue

--- gamma2024.client/src/components/views/TableauDeBordVentes.vue
+++ gamma2024.client/src/components/views/TableauDeBordVentes.vue
@@ -10,47 +10,76 @@
                    type="search"
                    aria-label="Recherche" /><label for="Recherche">Rechercher: </label>
         </div>
-
-        <table class="table table-striped mt-5 mx-0 text-center">
-            <thead>
-                <tr>
-                    <th data-field="dateAchat">Date de l'achat</th>
-                    <th data-field="prixFinal">Prix final</th>
-                    <th data-field="fraisEncanteur">Frais d'encanteur</th>
-                    <th data-field="fraisLivraison">Frais de livraison</th>
-                    <th data-field="tps">TPS</th>
-                    <th data-field="tvq">TVQ</th>
-                    <th data-field="don">Don</th>
-                    <th data-field="client">Client</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="align-middle">
-                        <span>
-                            <button class="btn btn-info">
-                                <img src="/images/ice.png" class="img-fluid" alt="..." />
-                            </button>
-                        </span>
-
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="card cardtest">
+            <Tabs value="0">
+                <TabList>
+                    <Tab class="tab" @click="voirTabs='parEncan'">Par encan</Tab>
+                    <Tab class="tab" @click="voirTabs='parClient'">Par client</Tab>
+                    <Tab class="tab" @click="voirTabs='parDate'">Par date</Tab>
+                </TabList>
+                <TabPanels v-if="voirTabs=='parEncan'">
+                    <TabPanel>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Encan</th>
+                                    <th scope="col">Client</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>
+                                        <button class="btn btn-info" @click="detailsDuMembre(membre.id)">
+                                            <img src="/images/ice.png" class="img-fluid" alt="..." />
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </TabPanel>
+                </TabPanels>
+                <TabPanels v-if="voirTabs=='parClient'">
+                    <TabPanel>
+                        <p class="m-0">bb</p>
+                    </TabPanel>
+                </TabPanels>
+                <TabPanels v-if="voirTabs=='parDate'">
+                    <TabPanel>
+                        <p class="m-0">cc</p>
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
+        </div>
     </div>
 </template>
 
 <script setup>
+    import Tabs from 'primevue/tabs';
+    import TabList from 'primevue/tablist';
+    import Tab from 'primevue/tab';
+    import TabPanels from 'primevue/tabpanels';
+    import TabPanel from 'primevue/tabpanel';
+    import { onMounted, ref } from "vue";
 
+    const voirTabs = ref("");
+
+    onMounted(async () => {
+        voirTabs.value = "parEncan";
+
+        var btns = document.getElementsByClassName("tab");
+        for (var i = 0; i < btns.length; i++) {
+            btns[0].className += " active";
+
+            btns[i].addEventListener("click", function () {
+                var current = document.getElementsByClassName("active");
+                current[0].className = current[0].className.replace(" active", "");
+                this.className += " active";
+            });
+        }
+    });
 </script>
 
 <style scoped>
@@ -70,5 +99,38 @@
 
     td {
         font-size: 16px;
+    }
+
+    .p-tab {
+        text-align: center;
+        border-radius: 10px 10px 0px 0px;
+        width: 100px;
+        margin-right: 1px;
+        border: none;
+        background-color: #5A708A;
+        color: white;
+    }
+
+    .p-tab:hover {
+        background-color: #83a0ba;
+        color: white;
+    }
+
+    .p-tab:not(.p-tab-active):not(.p-disabled):hover{
+        color: white;
+        background-color: #83a0ba;
+    }
+
+    .active {
+        background-color: #1C3755;
+    }
+
+    .cardtest {
+        --bs-card-border-color: none;
+        margin: 10px;
+    }
+
+    table {
+        box-shadow: none;
     }
 </style>
