4c6179ee4389b58854e14c56fc7f508ec1d6c438
AdaNeil <2135621@CegepGranby.qc.ca>
2024-11-05 11:53:51 -0500

    Ajout du route vers la page de Paiement d'un facture

--- gamma2024.client/src/router/index.js
+++ gamma2024.client/src/router/index.js
@@ -26,217 +26,227 @@ import ReinitialisationMotDePasse from "@/components/views/ReinitialisationMotDe
 import TableauDeBordEncans from "@/components/views/TableauDeBordEncans.vue";
 import TableauDeBordEncansAjout from "@/components/views/TableauDeBordEncansAjout.vue";
 import ModificationEncan from "@/components/views/ModificationEncan.vue";
+import Paiement from "@/components/views/Paiement.vue";
 const routes = [
-  {
-    path: "/",
-    name: "Accueil",
-    component: Accueil,
-    meta: { requiresAuth: false },
-  },
-  {
-    path: "/accueil",
-    name: "AccueilPage",
-    component: Accueil,
-    meta: { requiresAuth: false },
-  },
-  {
-    path: "/contact",
-    name: "Contact",
-    component: Contact,
-    meta: { requiresAuth: false },
-  },
-  {
-    path: "/apropos",
-    name: "APropos",
-    component: APropos,
-    meta: { requiresAuth: false },
-  },
-  {
-    path: "/connexion",
-    name: "Connexion",
-    component: Connexion,
-    meta: { requiresAuth: false },
-  },
-  {
-    path: "/inscription",
-    name: "Inscription",
-    component: Inscription,
-    meta: { requiresAuth: false },
-  },
-  {
-    path: "/encanpresent",
-    name: "EncanPresent",
-    component: EncanPresent,
-    meta: { requiresAuth: false },
-  },
-  {
-    path: "/touslesencans",
-    name: "TousLesEncans",
-    component: TousLesEncans,
-    meta: { requiresAuth: false },
-  },
-  {
-    path: "/encansfuturs",
-    name: "EncansFuturs",
-    component: EncansFuturs,
-    meta: { requiresAuth: false },
-  },
-  {
-    path: "/encanspasses",
-    name: "EncansPasses",
-    component: EncansPasses,
-    meta: { requiresAuth: false },
-  },
-  {
-    path: "/detailslot/:idLot",
-    name: "DetailsLot",
-    component: DetailsLot,
-    props: true,
-    meta: { requiresAuth: false },
-  },
-  {
-    path: "/inventaire",
-    name: "TableauDeBordInventaire",
-    component: TableauDeBordInventaire,
-    meta: { requiresAuth: true, requiredRole: "Administrateur" },
-  },
-  {
-    path: "/modificationprofilutilisateur",
-    name: "ModificationProfilUtilisateur",
-    component: () => ModificationProfilUtilisateur,
-    meta: { requiresAuth: true, requiredRole: "Client" },
-    beforeEnter: async (to, from, next) => {
-      if (store.state.isLoggedIn) {
-        try {
-          await store.dispatch("fetchClientInfo");
-          next();
-        } catch (error) {
-          console.error(
-            "Erreur lors du chargement des informations client:",
-            error
-          );
-          next("/error");
-        }
-      } else {
-        next("/login");
-      }
+    {
+        path: "/",
+        name: "Accueil",
+        component: Accueil,
+        meta: { requiresAuth: false },
     },
-  },
-  {
-    path: "/affichagevendeurs",
-    name: "AffichageVendeurs",
-    component: AffichageVendeur,
-    meta: { requiresAuth: true, requiredRole: "Administrateur" },
-  },
-  {
-    path: "/vendeurcreation",
-    name: "VendeurCreation",
-    component: VendeurCreation,
-    meta: { requiresAuth: true, requiredRole: "Administrateur" },
-  },
-  {
-    path: "/vendeurmodification/:id",
-    name: "vendeurModification",
-    component: VendeurModification,
-    props: true, // Ceci permet de passer l'ID comme prop au composant
-    meta: { requiresAuth: true, requiredRole: "Administrateur" },
-  },
-  {
-    path: "/accesnonautorise",
-    name: "AccesNonAutorise",
-    component: AccesNonAutorise,
-    meta: { requiresAuth: false },
-  },
-  {
-    path: "/affichagelots",
-    name: "AffichageLots",
-    component: AffichageLots,
-    meta: { requiresAuth: true, requiredRole: "Administrateur" },
-  },
-  {
-    path: "/lots/creation",
-    name: "CreationLot",
-    component: CreationLot,
-    meta: { requiresAuth: true, requiredRole: "Administrateur" },
-  },
-  {
-    path: "/lots/modification/:id",
-    name: "ModificationLot",
-    component: ModificationLot,
-    props: true,
-    meta: { requiresAuth: true, requiredRole: "Administrateur" },
-  },
-  {
-    path: "/gestionMembre",
-    name: "GestionMembre",
-    component: GestionMembreParAdmin,
-    meta: { requiresAuth: true, requiredRole: "Administrateur" },
-  },
-  {
-    path: "/detailsMembre/:id",
-    name: "detailsMembre",
-    component: DetailsMembreParAdmin,
-    props: true,
-    meta: { requiresAuth: true, requiredRole: "Administrateur" },
-  },
-  {
-    path: "/encan/:numeroEncan",
-    name: "Encan",
-    component: Encan,
-    props: true,
-    meta: { requiresAuth: false },
-  },
-  {
-    path: "/tableaudebordencans",
-    name: "TableauDeBordEncans",
-    component: TableauDeBordEncans,
-    meta: { requiresAuth: true, requiredRole: "Administrateur" },
-  },
-  {
-    path: "/tableaudebordencansajout",
-    name: "TableauDeBordEncansAjout",
-    component: TableauDeBordEncansAjout,
-    meta: { requiresAuth: true, requiredRole: "Administrateur" },
-  },
-  {
-    path: "/ModificationEncan/:id",
-    name: "ModificationEncan",
-    component: ModificationEncan,
-    meta: { requiresAuth: true, requiredRole: "Administrateur" },
-  },
-  {
-    path: "/ReinitialiserMotDePasse",
-    name: "ReinitialiserMotDePasse",
-    component: ReinitialisationMotDePasse,
-  },
+    {
+        path: "/accueil",
+        name: "AccueilPage",
+        component: Accueil,
+        meta: { requiresAuth: false },
+    },
+    {
+        path: "/contact",
+        name: "Contact",
+        component: Contact,
+        meta: { requiresAuth: false },
+    },
+    {
+        path: "/apropos",
+        name: "APropos",
+        component: APropos,
+        meta: { requiresAuth: false },
+    },
+    {
+        path: "/connexion",
+        name: "Connexion",
+        component: Connexion,
+        meta: { requiresAuth: false },
+    },
+    {
+        path: "/inscription",
+        name: "Inscription",
+        component: Inscription,
+        meta: { requiresAuth: false },
+    },
+    {
+        path: "/encanpresent",
+        name: "EncanPresent",
+        component: EncanPresent,
+        meta: { requiresAuth: false },
+    },
+    {
+        path: "/touslesencans",
+        name: "TousLesEncans",
+        component: TousLesEncans,
+        meta: { requiresAuth: false },
+    },
+    {
+        path: "/encansfuturs",
+        name: "EncansFuturs",
+        component: EncansFuturs,
+        meta: { requiresAuth: false },
+    },
+    {
+        path: "/encanspasses",
+        name: "EncansPasses",
+        component: EncansPasses,
+        meta: { requiresAuth: false },
+    },
+    {
+        path: "/detailslot/:idLot",
+        name: "DetailsLot",
+        component: DetailsLot,
+        props: true,
+        meta: { requiresAuth: false },
+    },
+    {
+        path: "/inventaire",
+        name: "TableauDeBordInventaire",
+        component: TableauDeBordInventaire,
+        meta: { requiresAuth: true, requiredRole: "Administrateur" },
+    },
+    {
+        path: "/modificationprofilutilisateur",
+        name: "ModificationProfilUtilisateur",
+        component: () => ModificationProfilUtilisateur,
+        meta: { requiresAuth: true, requiredRole: "Client" },
+        beforeEnter: async (to, from, next) => {
+            if (store.state.isLoggedIn) {
+                try {
+                    await store.dispatch("fetchClientInfo");
+                    next();
+                } catch (error) {
+                    console.error(
+                        "Erreur lors du chargement des informations client:",
+                        error
+                    );
+                    next("/error");
+                }
+            } else {
+                next("/login");
+            }
+        },
+    },
+    {
+        path: "/affichagevendeurs",
+        name: "AffichageVendeurs",
+        component: AffichageVendeur,
+        meta: { requiresAuth: true, requiredRole: "Administrateur" },
+    },
+    {
+        path: "/vendeurcreation",
+        name: "VendeurCreation",
+        component: VendeurCreation,
+        meta: { requiresAuth: true, requiredRole: "Administrateur" },
+    },
+    {
+        path: "/vendeurmodification/:id",
+        name: "vendeurModification",
+        component: VendeurModification,
+        props: true, // Ceci permet de passer l'ID comme prop au composant
+        meta: { requiresAuth: true, requiredRole: "Administrateur" },
+    },
+    {
+        path: "/accesnonautorise",
+        name: "AccesNonAutorise",
+        component: AccesNonAutorise,
+        meta: { requiresAuth: false },
+    },
+    {
+        path: "/affichagelots",
+        name: "AffichageLots",
+        component: AffichageLots,
+        meta: { requiresAuth: true, requiredRole: "Administrateur" },
+    },
+    {
+        path: "/lots/creation",
+        name: "CreationLot",
+        component: CreationLot,
+        meta: { requiresAuth: true, requiredRole: "Administrateur" },
+    },
+    {
+        path: "/lots/modification/:id",
+        name: "ModificationLot",
+        component: ModificationLot,
+        props: true,
+        meta: { requiresAuth: true, requiredRole: "Administrateur" },
+    },
+    {
+        path: "/gestionMembre",
+        name: "GestionMembre",
+        component: GestionMembreParAdmin,
+        meta: { requiresAuth: true, requiredRole: "Administrateur" },
+    },
+    {
+        path: "/detailsMembre/:id",
+        name: "detailsMembre",
+        component: DetailsMembreParAdmin,
+        props: true,
+        meta: { requiresAuth: true, requiredRole: "Administrateur" },
+    },
+    {
+        path: "/encan/:numeroEncan",
+        name: "Encan",
+        component: Encan,
+        props: true,
+        meta: { requiresAuth: false },
+    },
+    {
+        path: "/tableaudebordencans",
+        name: "TableauDeBordEncans",
+        component: TableauDeBordEncans,
+        meta: { requiresAuth: true, requiredRole: "Administrateur" },
+    },
+    {
+        path: "/tableaudebordencansajout",
+        name: "TableauDeBordEncansAjout",
+        component: TableauDeBordEncansAjout,
+        meta: { requiresAuth: true, requiredRole: "Administrateur" },
+    },
+    {
+        path: "/ModificationEncan/:id",
+        name: "ModificationEncan",
+        component: ModificationEncan,
+        meta: { requiresAuth: true, requiredRole: "Administrateur" },
+    },
+    {
+        path: "/ReinitialiserMotDePasse",
+        name: "ReinitialiserMotDePasse",
+        component: ReinitialisationMotDePasse,
+    },
+    {
+        path: "/Paiement/:idFacture",
+        name: "Paiement",
+        component: Paiement,
+        props: true,
+        meta: {
+            requiresAuth: false
+        },
+    }
 ];
 
 const router = createRouter({
-  history: createWebHistory("/2162067"),
-  routes,
-  scrollBehavior(to, from, savedPosition) {
-    return { top: 0 };
-  },
+    history: createWebHistory("/2162067"),
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        return { top: 0 };
+    },
 });
 
 router.beforeEach(async (to, from, next) => {
-  if (!store.state.api) {
-    await store.dispatch("initializeStore");
-  }
+    if (!store.state.api) {
+        await store.dispatch("initializeStore");
+    }
 
-  const isLoggedIn = store.state.isLoggedIn;
-  const userRoles = store.state.roles;
+    const isLoggedIn = store.state.isLoggedIn;
+    const userRoles = store.state.roles;
 
-  if (to.meta.requiresAuth && !isLoggedIn) {
-    next("/connexion");
-  } else if (
-    to.meta.requiredRole &&
-    !userRoles.includes(to.meta.requiredRole)
-  ) {
-    next({ name: "AccesNonAutorise" });
-  } else {
-    next();
-  }
+    if (to.meta.requiresAuth && !isLoggedIn) {
+        next("/connexion");
+    } else if (
+        to.meta.requiredRole &&
+        !userRoles.includes(to.meta.requiredRole)
+    ) {
+        next({ name: "AccesNonAutorise" });
+    } else {
+        next();
+    }
 });
 
 export default router;
