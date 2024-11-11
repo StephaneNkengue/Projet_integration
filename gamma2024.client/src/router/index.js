38d9e443096f6cbf9218858cb32f38626c65d3ed
ThiMaiTrang <2162067@CegepGranby.qc.ca>
2024-11-11 11:25:09 -0500

    Modification du fichier index.js dans router
    Modification du createWebHistory() pour utiliser les variables d'environnement

--- gamma2024.client/src/router/index.js
+++ gamma2024.client/src/router/index.js
@@ -250,7 +250,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory("/2162067"),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes,
     scrollBehavior(to, from, savedPosition) {
         return { top: 0 };
