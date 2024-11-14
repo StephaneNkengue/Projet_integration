566539cd45325bb34987758a12b5d4d88f690e0e
AdaNeil <2135621@CegepGranby.qc.ca>
2024-11-14 14:19:47 -0500

    Modification de Facture.cs afin de prende en compte l'ajout de FactureLivraison.cs

--- Gamma2024.Server/Models/Facture.cs
+++ Gamma2024.Server/Models/Facture.cs
@@ -17,78 +17,41 @@ namespace Gamma2024.Server.Models
         private double _fraisEncanteur;
         public double FraisEncanteur { get => _fraisEncanteur; set => _fraisEncanteur = double.Round(value, 2, MidpointRounding.ToZero); }
 
-        private double _fraisLivraison;
-        public double FraisLivraison { get => _fraisLivraison; set => _fraisLivraison = double.Round(value, 2, MidpointRounding.ToZero); }
-
         private double _tPS;
         public double TPS { get => _tPS; set => _tPS = double.Round(value, 2, MidpointRounding.ToZero); }
 
         private double _tVQ;
         public double TVQ { get => _tVQ; set => _tVQ = double.Round(value, 2, MidpointRounding.ToZero); }
 
-        private double? _don;
-        public double? Don
-        {
-            get => _don;
-            set => _don = value.HasValue ? Math.Round(value.Value, 2, MidpointRounding.ToZero) : null;
-        }
-
-        public int IdAdresse { get; set; }
         public string IdClient { get; set; } = null!;
-        public int? IdCharite { get; set; }
         public ICollection<Lot> Lots { get; set; } = [];
-        public Adresse Adresse { get; set; } = null!;
-        public Charite? Charite { get; set; }
         public ApplicationUser Client { get; set; } = null!;
         public bool estPaye { get; set; } = false;
         public int NumeroEncan { get; set; }
+        public bool Livrable { get; set; } = false;
+
+        public int? IdFactureLivraison { get; set; }
+        public FactureLivraison? FactureLivraison { get; set; }
 
         public void CalculerFacture()
         {
             PrixLots = Lots.Select(l => l.Mise.Value).Sum();
             FraisEncanteur = PrixLots * 0.15;
             SousTotal = PrixLots + FraisEncanteur;
-            CalculerFraisLivraison();
 
-            if (IdCharite.HasValue)
-            {
-                double donCalcule = SousTotal * 0.02;
-                Don = Math.Round(donCalcule, 2, MidpointRounding.ToZero);
-                SousTotal += Don.Value; // Utilisation de .Value car on est sûr que Don a une valeur ici
-            }
-            else
-            {
-                Don = null;
-            }
 
             TPS = SousTotal * 0.05;
             TVQ = SousTotal * 0.09975;
 
-            PrixFinal = SousTotal + FraisLivraison + TPS + TVQ;
-        }
-        private void CalculerFraisLivraison()
-        {
-            var prix = new List<double>();
-            foreach (var item in Lots)
+            PrixFinal = SousTotal + TPS + TVQ;
+
+            if (Lots.Where(l => l.EstLivrable == false).Any())
             {
-                if (item.Hauteur <= 20 || item.Largeur <= 20)
-                {
-                    prix.Add(35);
-                }
-                else
-                {
-                    prix.Add(65);
-                }
-            }
-            if (prix.Any())
-            {
-                FraisLivraison = prix[0];
-                prix.RemoveAt(0);
-                FraisLivraison += (prix.Sum()) / 2;
+                Livrable = false;
             }
             else
             {
-                FraisLivraison = 0;
+                Livrable = true;
             }
         }
     }
