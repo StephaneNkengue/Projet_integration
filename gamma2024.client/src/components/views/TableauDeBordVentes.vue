6bbea3a584a9ff3f135c0c6626d577470f7cb84c
ThiMaiTrang <2162067@CegepGranby.qc.ca>
2024-11-15 09:11:48 -0500

    Ajout de la pagination dans la page

--- gamma2024.client/src/components/views/TableauDeBordVentes.vue
+++ gamma2024.client/src/components/views/TableauDeBordVentes.vue
@@ -8,6 +8,30 @@
                aria-label="Search" />
         <h1 class="text-center mt-5">Liste des ventes</h1>
 
+        <div class="d-flex flex-row gap-2 justify-content-end mb-3">
+            <button class="d-flex align-items-center text-center rounded btn bleuMoyenFond text-white btnSurvolerBleuMoyenFond btnDesactiverBleuMoyenFond"
+                    @click="changerNbVenteParPage(20)"
+                    v-bind:disabled="ventesParPage == 20">
+                20
+            </button>
+            <button class="d-flex align-items-center text-center rounded btn bleuMoyenFond text-white btnSurvolerBleuMoyenFond btnDesactiverBleuMoyenFond"
+                    @click="changerNbVenteParPage(50)"
+                    v-bind:disabled="ventesParPage == 50">
+                50
+            </button>
+            <button class="d-flex align-items-center text-center rounded btn bleuMoyenFond text-white btnSurvolerBleuMoyenFond btnDesactiverBleuMoyenFond"
+                    @click="changerNbVenteParPage(100)"
+                    v-bind:disabled="ventesParPage == 100">
+                100
+            </button>
+            <button class="d-flex align-items-center text-center rounded btn bleuMoyenFond text-white btnSurvolerBleuMoyenFond btnDesactiverBleuMoyenFond"
+                    type="button"
+                    @click="afficherTousVentes"
+                    v-bind:disabled="ventesParPage == nbVentesRecus">
+                Tous
+            </button>
+        </div>
+
         <div class="d-flex gap-2 justify-content-center" v-if="chargement">
             <div class="spinner-border" role="status">
                 <span class="visually-hidden">Chargement des ventes...</span>
@@ -40,7 +64,7 @@
                                  :class="{ show: encan == numerosEncans[0] }"
                                  data-bs-parent="#accordionEncan">
                                 <div class="accordion-body">
-                                    <div class="accordion" id="accordionClient" v-for="facture in filteredVentes.filter((x)=> x.encan == encan.encan)" :key="facture.id">
+                                    <div class="accordion" id="accordionClient" v-for="facture in ventesAffiche.filter((x)=> x.encan == encan.encan)" :key="facture.id">
                                         <div class="accordion-item">
                                             <h2 class="accordion-header px-0">
                                                 <button class="accordion-button d-flex"
@@ -54,7 +78,7 @@
                                                     </div>
                                                     <div class="col">
                                                         <button class="btn btn-info">
-                                                            <img src="/images/ice.png" class="img-fluid" alt="..." />
+                                                            <img src="/images/ice.png" class="btnVisuel img-fluid" alt="..." />
                                                         </button>
                                                     </div>
                                                 </button>
@@ -95,7 +119,31 @@
             </div>
         </div>
 
+        <div class="d-flex flex-row justify-content-center gap-1 flex-wrap p-3" v-if="ventesAffiche.length != 0">
+            <button type="button"
+                    class="btn bleuMoyenFond text-white btnSurvolerBleuMoyenFond btnDesactiverBleuMoyenFond"
+                    @click="reculerPage"
+                    v-bind:disabled="pageCourante == 1">
+                <
+            </button>
 
+            <div v-for="item in listePagination" :key="item.id">
+                <button type="button"
+                        class="btn bleuMoyenFond text-white btnSurvolerBleuMoyenFond btnDesactiverBleuMoyenFond"
+                        :pageId="item"
+                        @click="changerPage()"
+                        v-bind:disabled="pageCourante == item || item == '...'">
+                    {{ item }}
+                </button>
+            </div>
+
+            <button type="button"
+                    class="btn bleuMoyenFond text-white btnSurvolerBleuMoyenFond btnDesactiverBleuMoyenFond"
+                    @click="avancerPage"
+                    v-bind:disabled="pageCourante == nbPages">
+                >
+            </button>
+        </div>
     </div>
 </template>
 
@@ -110,6 +158,13 @@
     const styleBorder = ref('');
     const chargement = ref(true);
 
+    const pageCourante = ref(1);
+    const ventesParPage = ref();
+    const nbVentesRecus = ref();
+    const nbPages = ref();
+    const listePagination = ref([]);
+    const ventesAffiche = ref([]);
+
     onMounted(async () => {
         try {
             listeFactures.value = await store.dispatch("fetchFactureInfo");
@@ -119,6 +174,13 @@
                     numerosEncans.value.push(value);
                 }
             });
+
+            nbVentesRecus.value = filteredVentes.value.length;
+            ventesParPage.value = nbVentesRecus.value;
+            nbPages.value = recalculerNbPages();
+
+            genererListePagination();
+            chercherVentesAAfficher();
 
             chargement.value = false;
         } catch (error) {
@@ -155,7 +217,89 @@
         else {
             styleBorder.value = '2px solid green'
         }
+
+        nbVentesRecus.value = filteredVentes.value.length;
+        pageCourante.value = 1;
+        AjusterPagination();
     });
+
+    watch(pageCourante, () => {
+        genererListePagination();
+        chercherVentesAAfficher();
+    });
+
+    const changerNbVenteParPage = ref(function (nouvVentesParPage) {
+        ventesParPage.value = nouvVentesParPage;
+        nbPages.value = recalculerNbPages();
+        pageCourante.value = 1;
+        AjusterPagination();
+    });
+
+    const afficherTousVentes = ref(function () {
+        ventesParPage.value = nbVentesRecus.value;
+        nbPages.value = recalculerNbPages();
+        pageCourante.value = 1;
+        AjusterPagination();
+    });
+
+    const reculerPage = ref(function () {
+        if (pageCourante.value > 1) {
+            pageCourante.value--;
+        }
+    });
+
+    const avancerPage = ref(function () {
+        if (pageCourante.value < nbPages.value) {
+            pageCourante.value++;
+        }
+    });
+
+    const changerPage = ref(function () {
+        pageCourante.value = parseInt(event.srcElement.getAttribute("pageId"));
+    });
+
+    function recalculerNbPages() {
+        return Math.ceil(nbVentesRecus.value / ventesParPage.value);
+    }
+
+    function genererListePagination() {
+        listePagination.value = [];
+
+        for (let i = 1; i <= nbPages.value; i++) {
+            if (
+                i == 1 ||
+                (i >= pageCourante.value - 1 && i <= pageCourante.value + 1) ||
+                i == nbPages.value
+            ) {
+                listePagination.value.push(i);
+            } else if (
+                listePagination.value[listePagination.value.length - 1] != "..."
+            ) {
+                listePagination.value.push("...");
+            }
+        }
+    }
+
+    function AjusterPagination() {
+        nbPages.value = recalculerNbPages();
+        genererListePagination();
+        chercherVentesAAfficher();
+    }
+
+    function chercherVentesAAfficher() {
+        ventesAffiche.value = [];
+
+        let positionDebut = (pageCourante.value - 1) * ventesParPage.value;
+        let positionFin = pageCourante.value * ventesParPage.value;
+
+        for (
+            let i = positionDebut;
+            i < positionFin && i < filteredVentes.value.length;
+            i++
+        ) {
+            ventesAffiche.value.push(filteredVentes.value[i]);
+        }
+    }
 </script>
 
 <style scoped>
@@ -177,7 +321,7 @@
         font-size: 16px;
     }
 
-    .btn {
+    .btnVisuel {
         background-color: #0dcaf0;
     }
 
