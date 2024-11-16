6755860c3a32a964f37fd995f4df029e850597cd
AdaNeil <2135621@CegepGranby.qc.ca>
2024-11-16 16:20:17 -0500

    Ajout d'un spinner pour le chargement sur la page de gestion des cartes de credit par le client

--- gamma2024.client/src/components/views/GestionCartes.vue
+++ gamma2024.client/src/components/views/GestionCartes.vue
@@ -2,15 +2,23 @@
     <div class="container">
         <h1 class="text-center">Gestion des cartes de crédit</h1>
 
-        <router-link :to="{name: 'EnregistrerCarte'}">
-            <button class="btn btn-outline bleuMoyenFond text-white btnSurvolerBleuMoyenFond my-2">
-                Ajouter une carte
-            </button>
-        </router-link>
-        <div class="d-flex flex-wrap w-100 justify-content-between">
-            <div v-for="carte in cartes" class="col-6 p-3">
-                <AffichageCarteCredit :carte="carte"></AffichageCarteCredit>
+        <div v-if="chargement" class=text-center>
+            <div class="spinner-border" role="status">
+                <span class="sr-only"></span>
             </div>
+        </div>
+        <div v-else>
+            <router-link :to="{name: 'EnregistrerCarte'}">
+                <button class="btn btn-outline bleuMoyenFond text-white btnSurvolerBleuMoyenFond my-2">
+                    Ajouter une carte
+                </button>
+            </router-link>
+            <div class="d-flex flex-wrap w-100 justify-content-between">
+                <div v-for="carte in cartes" class="col-6 p-3">
+                    <AffichageCarteCredit :carte="carte"></AffichageCarteCredit>
+                </div>
+            </div>
+
         </div>
 
     </div>
@@ -22,11 +30,13 @@
     import { ref, onMounted } from "vue";
     const store = useStore();
     const cartes = ref([])
+    const chargement = ref(true)
 
     onMounted(async () => {
         try {
             const response = await store.dispatch("chercherCartesUser")
             cartes.value = response.data
+            chargement.value = false
         } catch (error) {
             console.error("Erreur lors de la récupération des lots:", error);
         }
