8944215a4c12dac0e43fce914ae80272c4d604dd
ThiMaiTrang <2162067@CegepGranby.qc.ca>
2024-11-18 10:48:56 -0500

    Ajout de la page de l'historique des achats d'un membre
    Ajout de la route
    Ajout de l'onglet dans la barre de navigation

---
+++ gamma2024.client/src/components/views/HistoriqueAchatsParMembre.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container mt-5">
+        <h3 class="text-center">Rechercher une vente</h3>
+
+        <input v-model="searchQuery"
+               class="form-control row col-10 ms-1"
+               type="search"
+               placeholder="Rechercher une vente"
+               aria-label="Search" />
+
+        <h1 class="text-center mt-5">Liste des achats</h1>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
