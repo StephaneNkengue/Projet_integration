e44ff0bb1bbaee5056927498d066c23ce7218f90
AdaNeil <2135621@CegepGranby.qc.ca>
2024-11-20 13:32:14 -0500

    Ajout du code dans la page GestionCartes.vue pour permettre la suppression de cartes

--- gamma2024.client/src/components/views/GestionCartes.vue
+++ gamma2024.client/src/components/views/GestionCartes.vue
@@ -2,6 +2,9 @@
     <div class="container">
         <h1 class="text-center">Gestion des cartes de crédit</h1>
 
+        <div class="alert alert-danger" role="alert" id="message" v-show="siMessage">
+        </div>
+
         <div v-if="chargement" class=text-center>
             <div class="spinner-border" role="status">
                 <span class="sr-only"></span>
@@ -15,7 +18,7 @@
             </router-link>
             <div class="d-flex flex-wrap w-100 justify-content-between">
                 <div v-for="carte in cartes" class="col-6 p-3">
-                    <AffichageCarteCredit :carte="carte"></AffichageCarteCredit>
+                    <AffichageCarteCredit :carte="carte" @supprimerCarte="supprimerCarte"></AffichageCarteCredit>
                 </div>
             </div>
 
@@ -31,6 +34,7 @@
     const store = useStore();
     const cartes = ref([])
     const chargement = ref(true)
+    const siMessage = ref(false)
 
     onMounted(async () => {
         try {
@@ -41,5 +45,24 @@
             console.error("Erreur lors de la récupération des lots:", error);
         }
     });
+
+    const supprimerCarte = ref(async function (pmId) {
+        try {
+            if (cartes.value.length > 1) {
+                siMessage.value = false
+                const carteSuppression = await store.dispatch("supprimerCarte", pmId)
+                const response = await store.dispatch("chercherCartesUser")
+                cartes.value = response.data
+            }
+            else {
+                siMessage.value = true
+                document.querySelector("#message").innerHTML = "Vous devez avoir au moins une carte dans votre profil afin d'utiliser nos services"
+            }
+
+        } catch (error) {
+            siMessage.value = true
+            document.querySelector("#message").innerHTML = error
+        }
+    })
 </script>
 <style></style>
