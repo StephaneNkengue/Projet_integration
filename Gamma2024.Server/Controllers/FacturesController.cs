be27576151418a1e70ed1bb9d52cb8151dba793f
AdaNeil <2135621@CegepGranby.qc.ca>
2024-11-21 08:41:29 -0500

    Ajout du code semi-fonctionnel dans le controller afin de tester et avoir de l'assistance

--- Gamma2024.Server/Controllers/FacturesController.cs
+++ Gamma2024.Server/Controllers/FacturesController.cs
@@ -22,18 +22,19 @@ namespace Gamma2024.Server.Controllers
         private readonly IEmailSender _emailSender;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly FactureService _factureService;
+        private readonly IWebHostEnvironment _environment;
 
-        public FacturesController(IJsReportMVCService jsReportService, ApplicationDbContext context, UserManager<ApplicationUser> userManager, FactureService factureService, IEmailSender emailSender)
+        public FacturesController(IJsReportMVCService jsReportService, IWebHostEnvironment environment, ApplicationDbContext context, UserManager<ApplicationUser> userManager, FactureService factureService, IEmailSender emailSender)
         {
             _jsReportService = jsReportService;
             _context = context;
             _userManager = userManager;
             _factureService = factureService;
             _emailSender = emailSender;
+            _environment = environment;
         }
 
-        [HttpPost("generate")]
-        public async Task<IActionResult> GenerateInvoice([FromBody] FactureGenererVM model)
+        private async Task<IActionResult> GenerateInvoice([FromBody] FactureGenererVM model)
         {
             var report = await _jsReportService.RenderAsync(new RenderRequest
             {
@@ -51,9 +52,18 @@ namespace Gamma2024.Server.Controllers
                 }
             });
 
-            //File(report.Content, "application/pdf", "invoice.pdf");
+            //var pdf = File(report.Content, "application/pdf", "invoice.pdf");
             var memoryStream = new MemoryStream();
             await report.Content.CopyToAsync(memoryStream);
+
+            //Fonctionne mais on ne peut pas ouvrir le fichier
+            string uploadPath = Path.Combine(_environment.WebRootPath, "Factures");
+            string fileName = $"Facture232_{Guid.NewGuid()}.pdf";
+            string filePath = Path.Combine(uploadPath, fileName);
+            var stream = new FileStream(filePath, FileMode.Create, FileAccess.ReadWrite);
+
+            await report.Content.CopyToAsync(stream);
+
             var pdfBytes = memoryStream.ToArray();
 
             var client = await _context.Users.FindAsync(model.Client.ClientId);
@@ -63,7 +73,7 @@ namespace Gamma2024.Server.Controllers
                 var messageText = $"Merci d'avoir magasiné chez Encans de Nantes au Québec. Vous trouverez votre facture en pièce jointe.";
                 var attachmentName = $"FactureEncan{model.NumeroEncan}-EncansDeNantes.pdf";
                 _emailSender.Send(client.Email, subject, messageText, pdfBytes, attachmentName);
-                return Ok("Facture envoyé avec succès");
+                return Ok(pdfBytes);
             }
             else
             {
@@ -90,39 +100,44 @@ namespace Gamma2024.Server.Controllers
             return factures;
         }
         [HttpPost("CreerFacturesParEncan/{numeroEncan}")]
-        public ICollection<FactureGenererVM> CreerFacturesParEncan(int numeroEncan)
+        public async Task<IActionResult> CreerFacturesParEncan(int numeroEncan)
         {
             ICollection<Facture> factures = _factureService.CreerFacturesParEncan(numeroEncan);
 
-            var facturesGen = factures.Select(f => new FactureGenererVM
+            foreach (var f in factures)
             {
-                DateAchat = f.DateAchat,
-                FraisEncanteur = f.FraisEncanteur,
-                NumeroEncan = numeroEncan,
-                Id = f.Id,
-                TPS = f.TPS,
-                TVQ = f.TVQ,
-                Lots = f.Lots.Select(l => new LotFactureVM
+                var factureGenerer = new FactureGenererVM
                 {
-                    Description = l.Description,
-                    Prix = l.Mise.Value.ToString(),
-                }).ToList(),
-                Client = new ClientFactureVM
-                {
-                    AdresseLigne1 = $"{f.Client.Adresses.First().Numero} {f.Client.Adresses.First().Rue}",
-                    AdresseLigne2 = $"{f.Client.Adresses.First().Appartement}",
-                    AdresseLigne3 = $"{f.Client.Adresses.First().Ville}, {f.Client.Adresses.First().Province}, {f.Client.Adresses.First().Pays}",
-                    CodePostal = f.Client.Adresses.First().CodePostal,
-                    Courriel = f.Client.Email,
-                    Nom = $"{f.Client.FirstName} {f.Client.Name}",
-                    Telephone = f.Client.PhoneNumber,
-                    ClientId = f.Client.Id,
-                },
-                PrixFinal = f.PrixFinal,
-                SousTotal = f.SousTotal
-            });
+                    DateAchat = f.DateAchat,
+                    FraisEncanteur = f.FraisEncanteur,
+                    NumeroEncan = numeroEncan,
+                    Id = f.Id,
+                    TPS = f.TPS,
+                    TVQ = f.TVQ,
+                    Lots = f.Lots.Select(l => new LotFactureVM
+                    {
+                        Description = l.Description,
+                        Prix = l.Mise.Value.ToString(),
+                    }).ToList(),
+                    Client = new ClientFactureVM
+                    {
+                        AdresseLigne1 = $"{f.Client.Adresses.First().Numero} {f.Client.Adresses.First().Rue}",
+                        AdresseLigne2 = $"{f.Client.Adresses.First().Appartement}",
+                        AdresseLigne3 = $"{f.Client.Adresses.First().Ville}, {f.Client.Adresses.First().Province}, {f.Client.Adresses.First().Pays}",
+                        CodePostal = f.Client.Adresses.First().CodePostal,
+                        Courriel = f.Client.Email,
+                        Nom = $"{f.Client.FirstName} {f.Client.Name}",
+                        Telephone = f.Client.PhoneNumber,
+                        ClientId = f.Client.Id,
+                    },
+                    PrixFinal = f.PrixFinal,
+                    SousTotal = f.SousTotal
+                };
 
-            return facturesGen.ToList();
+                var invoiceGen = await GenerateInvoice(factureGenerer);
+            }
+
+            return Ok(factures);
         }
     }
 }
