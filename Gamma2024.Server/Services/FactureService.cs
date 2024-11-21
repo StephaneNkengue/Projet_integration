902631d6634d5164e99dc40a2a9618a17c2fe88f
AdaNeil <2135621@CegepGranby.qc.ca>
2024-11-21 18:58:47 -0500

    Ajout du méthode de paiement de la facture dans le service des factures afin de faciliter l'appel

--- Gamma2024.Server/Services/FactureService.cs
+++ Gamma2024.Server/Services/FactureService.cs
@@ -2,6 +2,7 @@ using Gamma2024.Server.Data;
 using Gamma2024.Server.Models;
 using Gamma2024.Server.ViewModels;
 using Microsoft.EntityFrameworkCore;
+using Stripe;
 
 namespace Gamma2024.Server.Services
 {
@@ -86,7 +87,8 @@ namespace Gamma2024.Server.Services
                             DateAchat = lot.DateFinVente.Value,
                             Lots = [lot],
                             Client = lot.ClientMise,
-                            NumeroEncan = numeroEncan
+                            NumeroEncan = numeroEncan,
+                            FacturePDFPath = ""
                         });
                     }
                 }
@@ -99,6 +101,139 @@ namespace Gamma2024.Server.Services
             return factures;
         }
 
+        public string ChargerFacture(Facture facture)
+        {
+            var productService = new ProductService();
+            var invoiceService = new InvoiceService();
+            var invoiceItemService = new InvoiceItemService();
+            var paymentMethodService = new PaymentMethodService();
+            var chargeService = new ChargeService();
+
+            if (!facture.estPaye)
+            {
+                var customerId = facture.Client.StripeCustomer;
+                var paymentMethodOptions = new PaymentMethodListOptions
+                {
+                    Type = "card",
+                    Limit = 1,
+                    Customer = customerId,
+                };
+
+                StripeList<PaymentMethod> paymentMethods = paymentMethodService.List(paymentMethodOptions);
+
+                var invoiceOptions = new InvoiceCreateOptions
+                {
+                    Customer = customerId,
+                    CollectionMethod = "charge_automatically",
+                    DefaultPaymentMethod = paymentMethods.Data[0].Id,
+                    AutomaticTax = new InvoiceAutomaticTaxOptions(),
+                };
+
+                var invoice = invoiceService.Create(invoiceOptions);
+
+                foreach (var lot in facture.Lots)
+                {
+                    var productOptions = new ProductCreateOptions
+                    {
+                        Name = lot.Description,
+                        DefaultPriceData = new ProductDefaultPriceDataOptions
+                        {
+                            UnitAmount = (long)(lot.Mise.Value * 100),
+                            Currency = "cad",
+                        },
+                        Expand = new List<string> { "default_price" },
+                    };
+                    var product = productService.Create(productOptions);
+
+                    var invoiceItemOptions = new InvoiceItemCreateOptions
+                    {
+                        Customer = customerId,
+                        Invoice = invoice.Id,
+                        Description = lot.Description,
+                        Price = product.DefaultPriceId
+                    };
+
+                    invoiceItemService.Create(invoiceItemOptions);
+                }
+
+                var fraisProdOptions = new ProductCreateOptions
+                {
+                    Name = "Frais d'encanteur",
+                    DefaultPriceData = new ProductDefaultPriceDataOptions
+                    {
+                        UnitAmount = (long)(facture.FraisEncanteur * 100),
+                        Currency = "cad",
+                    },
+                    Expand = new List<string> { "default_price" },
+                };
+                var fraisProd = productService.Create(fraisProdOptions);
+
+                var fraisInvoiceItemOptions = new InvoiceItemCreateOptions
+                {
+                    Customer = customerId,
+                    Invoice = invoice.Id,
+                    Description = "Frais d'encanteur",
+                    Price = fraisProd.DefaultPriceId
+                };
+
+                invoiceItemService.Create(fraisInvoiceItemOptions);
+
+                var tpsProdOptions = new ProductCreateOptions
+                {
+                    Name = "TPS",
+                    DefaultPriceData = new ProductDefaultPriceDataOptions
+                    {
+                        UnitAmount = (long)(facture.TPS * 100),
+                        Currency = "cad",
+                    },
+                    Expand = new List<string> { "default_price" },
+                };
+                var tpsProd = productService.Create(tpsProdOptions);
+
+                var tpsInvoiceItemOptions = new InvoiceItemCreateOptions
+                {
+                    Customer = customerId,
+                    Invoice = invoice.Id,
+                    Description = "TPS",
+                    Price = tpsProd.DefaultPriceId
+                };
+
+                invoiceItemService.Create(tpsInvoiceItemOptions);
+
+                var tvqProdOptions = new ProductCreateOptions
+                {
+                    Name = "TVQ",
+                    DefaultPriceData = new ProductDefaultPriceDataOptions
+                    {
+                        UnitAmount = (long)(facture.TVQ * 100),
+                        Currency = "cad",
+                    },
+                    Expand = new List<string> { "default_price" },
+                };
+                var tvqProd = productService.Create(tvqProdOptions);
+
+                var tvqInvoiceItemOptions = new InvoiceItemCreateOptions
+                {
+                    Customer = customerId,
+                    Invoice = invoice.Id,
+                    Description = "TVQ",
+                    Price = tvqProd.DefaultPriceId
+                };
+
+                invoiceItemService.Create(tvqInvoiceItemOptions);
+
+                var invoicePaye = invoiceService.Pay(invoice.Id);
+
+                var charge = chargeService.Get(invoicePaye.ChargeId);
+
+                PayerFacture(facture.Id);
+
+                return charge.ReceiptUrl;
+            }
+
+            return "";
+        }
+
         public bool PayerFacture(int idFacture)
         {
             var facture = _context.Factures.FirstOrDefault(f => f.Id == idFacture);
@@ -109,6 +244,19 @@ namespace Gamma2024.Server.Services
             }
 
             facture.estPaye = true;
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool SauvegarderPDFFacture(int idFacture, string pathFacture)
+        {
+            var facture = _context.Factures.FirstOrDefault(f => f.Id == idFacture);
+            if (facture == null)
+            {
+                return false;
+            }
+            facture.FacturePDFPath = pathFacture;
+            _context.Factures.Update(facture);
             _context.SaveChanges();
             return true;
         }
