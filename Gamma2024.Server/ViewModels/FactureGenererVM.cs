24521761dd95d898500958b400803dd549782385
AdaNeil <2135621@CegepGranby.qc.ca>
2024-11-21 18:59:24 -0500

    Ajout d'un attribut pouvant contenir le lien du recu stripe dans le VM des factures

--- Gamma2024.Server/ViewModels/FactureGenererVM.cs
+++ Gamma2024.Server/ViewModels/FactureGenererVM.cs
@@ -12,6 +12,7 @@ namespace Gamma2024.Server.ViewModels
         public ICollection<LotFactureVM> Lots { get; set; } = [];
         public ClientFactureVM Client { get; set; } = null!;
         public int NumeroEncan { get; set; }
+        public string UrlInvoiceStripe { get; set; } = null!;
     }
 
     public class ClientFactureVM
