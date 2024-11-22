e4f880c921043eb8ddb6f90080058c410205f74b
AdaNeil <2135621@CegepGranby.qc.ca>
2024-11-21 23:17:27 -0500

    Création du modal qui permet d'afficher une facture

---
+++ gamma2024.client/src/components/modals/VoirFactureModal.vue
@@ -0,0 +1,51 @@
+<template lang="">
+    <div class="modal fade"
+         :id="props.idFacture"
+         data-bs-backdrop="static"
+         data-bs-keyboard="false"
+         tabindex="-1"
+         aria-labelledby="staticBackdropLabel"
+         aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content min-vh-75">
+                <div class="modal-header">
+                    <a :href="pdf" target="_blank">Voir le pdf dans un nouveau onglet</a>
+                    <button type="button"
+                            class="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"></button>
+                </div>
+                <div class="modal-body d-flex justify-content-center h-100">
+                    <iframe :src="pdf" class="w-100 min-vh-50" scrolling="no" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { ref, onMounted } from "vue";
+    import { useStore } from "vuex";
+
+    const store = useStore();
+
+    const props = defineProps({
+        facturePdfPath: String,
+        idFacture: Number
+    });
+
+    const pdf = ref(props.facturePdfPath)
+    onMounted(async () => {
+        const response = await store.state.api.defaults.baseURL.replace("/api", "")
+        pdf.value = response + props.facturePdfPath
+    });
+</script>
+<style scoped>
+    .min-vh-75 {
+        min-height: 75vh
+    }
+
+    .min-vh-50 {
+        min-height: 50vh
+    }
+</style>
