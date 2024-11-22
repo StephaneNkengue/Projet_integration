ff9ffe6c42726532c4460fd816b8190c80f085ae
AdaNeil <2135621@CegepGranby.qc.ca>
2024-11-22 10:49:28 -0500

    Suppression d'une partie du méthode de création de factures par encan dans le controller des factures

--- Gamma2024.Server/Controllers/FacturesController.cs
+++ Gamma2024.Server/Controllers/FacturesController.cs
@@ -4,12 +4,9 @@ using Gamma2024.Server.Models;
 using Gamma2024.Server.Services;
 using Gamma2024.Server.ViewModels;
 using jsreport.AspNetCore;
-using jsreport.Types;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using NuGet.Protocol;
 using System.Security.Claims;
 
 namespace Gamma2024.Server.Controllers
@@ -41,6 +38,7 @@ namespace Gamma2024.Server.Controllers
             ICollection<FactureAffichageVM> factures = _factureService.ChercherFactures();
             return factures;
         }
+
         [Authorize(Roles = "Client")]
         [HttpGet("chercherFacturesMembre")]
         public ICollection<FactureAffichageMembreVM> ChercherFacturesMembre()
@@ -53,50 +51,11 @@ namespace Gamma2024.Server.Controllers
             ICollection<FactureAffichageMembreVM> factures = _factureService.ChercherFacturesMembre(userId);
             return factures;
         }
+
         [HttpPost("CreerFacturesParEncan/{numeroEncan}")]
         public async Task<IActionResult> CreerFacturesParEncan(int numeroEncan)
         {
-            ICollection<Facture> factures = _factureService.CreerFacturesParEncan(numeroEncan);
-
-            foreach (var f in factures)
-            {
-                var urlRecuStripe = _factureService.ChargerFacture(f);
-                if (!urlRecuStripe.IsNullOrEmpty())
-                {
-                    var factureGenerer = new FactureGenererVM
-                    {
-                        DateAchat = f.DateAchat,
-                        FraisEncanteur = f.FraisEncanteur,
-                        NumeroEncan = numeroEncan,
-                        Id = f.Id,
-                        TPS = f.TPS,
-                        TVQ = f.TVQ,
-                        Lots = f.Lots.Select(l => new LotFactureVM
-                        {
-                            Description = l.Description,
-                            Prix = l.Mise.Value.ToString(),
-                        }).ToList(),
-                        Client = new ClientFactureVM
-                        {
-                            AdresseLigne1 = $"{f.Client.Adresses.First().Numero} {f.Client.Adresses.First().Rue}",
-                            AdresseLigne2 = $"{f.Client.Adresses.First().Appartement}",
-                            AdresseLigne3 = $"{f.Client.Adresses.First().Ville}, {f.Client.Adresses.First().Province}, {f.Client.Adresses.First().Pays}",
-                            CodePostal = f.Client.Adresses.First().CodePostal,
-                            Courriel = f.Client.Email,
-                            Nom = $"{f.Client.FirstName} {f.Client.Name}",
-                            Telephone = f.Client.PhoneNumber,
-                            ClientId = f.Client.Id,
-                        },
-                        PrixFinal = f.PrixFinal,
-                        SousTotal = f.SousTotal,
-                        UrlInvoiceStripe = urlRecuStripe
-                    };
-
-                    var invoiceGen = await GenerateInvoice(factureGenerer);
-                    _factureService.SauvegarderPDFFacture(f.Id, invoiceGen);
-                }
-            }
-
+            ICollection<Facture> factures = await _factureService.CreerFacturesParEncan(numeroEncan);
             return Ok(factures);
         }
 
