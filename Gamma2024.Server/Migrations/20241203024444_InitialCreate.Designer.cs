bca47a801d22956713dea451740c15d8d04eefc7
RENÉ-MARCELLIN TCHOKOMI <2136882@CegepGranby.qc.ca>
2024-12-02 21:46:54 -0500

    Création BD

--- Gamma2024.Server/Migrations/20241203024444_InitialCreate.Designer.cs
+++ Gamma2024.Server/Migrations/20241203024444_InitialCreate.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Gamma2024.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20241202145247_AjoutProprieteEstEnSoireeClotureEncan")]
-    partial class AjoutProprieteEstEnSoireeClotureEncan
+    [Migration("20241203024444_InitialCreate")]
+    partial class InitialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
