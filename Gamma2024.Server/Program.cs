9fafd82f607042c41a67e452165d5b18f579d050
RENÉ-MARCELLIN TCHOKOMI <2136882@CegepGranby.qc.ca>
2024-12-03 12:29:44 -0500

    feat(soiree-cloture): Implémentation du processus de fin de soirée automatique
    - Ajout de la surveillance de fin des lots
    - Appel séquentiel de CreerFacturesParEncan puis ChargerClients
    - Gestion de la redirection post-traitement
    - Réinitialisation de l'état après finalisation

--- Gamma2024.Server/Program.cs
+++ Gamma2024.Server/Program.cs
@@ -49,6 +49,7 @@ builder.Services.AddSignalR();
 builder.Services.AddScoped<ClientInscriptionService>();
 builder.Services.AddScoped<ClientModificationService>();
 builder.Services.AddScoped<EncanService>();
+builder.Services.AddHttpClient<EncanService>();
 builder.Services.AddScoped<VendeurService>();
 builder.Services.AddScoped<AdministrateurService>();
 builder.Services.AddScoped<LotService>();
