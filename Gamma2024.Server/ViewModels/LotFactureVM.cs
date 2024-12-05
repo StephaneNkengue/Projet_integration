36d5bd0739145a3e8a34ccac4f44bfbec2c2d1e5
AdaNeil <2135621@CegepGranby.qc.ca>
2024-12-05 16:45:43 -0500

    Changement du attribut prix dans le vm de lot pour les factures pour un double a la place d'un string

--- Gamma2024.Server/ViewModels/LotFactureVM.cs
+++ Gamma2024.Server/ViewModels/LotFactureVM.cs
@@ -1,8 +1,8 @@
 namespace Gamma2024.Server.ViewModels
 {
-	public class LotFactureVM
-	{
-		public string Description { get; set; } = default!;
-		public string Prix { get; set; } = default!;
-	}
+    public class LotFactureVM
+    {
+        public string Description { get; set; } = default!;
+        public double Prix { get; set; } = default!;
+    }
 }
