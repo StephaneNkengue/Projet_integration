650481e676de40f663f95f3de315baa9a97aa84c
AdaNeil <2135621@CegepGranby.qc.ca>
2024-12-05 14:58:36 -0500

    Rendre charité nullable dans le vm des details d'un facture de livraison

--- Gamma2024.Server/ViewModels/FactureLivraisonDetailsVM.cs
+++ Gamma2024.Server/ViewModels/FactureLivraisonDetailsVM.cs
@@ -12,6 +12,6 @@ namespace Gamma2024.Server.ViewModels
         public double? Don { get; set; } = 0;
         public ClientFactureVM Client { get; set; } = null!;
         public int NumeroEncan { get; set; }
-        public string Charite { get; set; } = default!;
+        public string? Charite { get; set; } = default!;
     }
 }
