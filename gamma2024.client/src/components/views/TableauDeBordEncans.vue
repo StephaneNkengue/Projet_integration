df9658ca9de211aad6523276d8a197c1ba74301e
ThiMaiTrang <2162067@CegepGranby.qc.ca>
2024-12-05 15:26:27 -0500

    Résolution du problème de fuseau horaire
    Modification de l'affichage de la date de la soirée de clôture (ce n'était pas la bonne variable qui était appelée)

--- gamma2024.client/src/components/views/TableauDeBordEncans.vue
+++ gamma2024.client/src/components/views/TableauDeBordEncans.vue
@@ -123,7 +123,7 @@
                             <td class="align-middle">{{ encan.dateDebut.split("T")[0] }}</td>
                             <td class="align-middle">{{ encan.dateFin.split("T")[0] }}</td>
                             <td class="align-middle">
-                                {{ encan.dateDebut.split("T")[0] }}
+                                {{ encan.dateDebutSoireeCloture.split("T")[0] }}
                                 {{ encan.dateDebutSoireeCloture.split("T")[1].split(".")[0] }}
                             </td>
                             <td class="align-middle">{{ encan.nbLots }}</td>
