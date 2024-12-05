0b9ff9cf040f0b2d4b099bce86ec5a6d83ef9f47
AdaNeil <2135621@CegepGranby.qc.ca>
2024-12-05 14:06:18 -0500

    Modification du modal pour afficher les factures normales

--- gamma2024.client/src/components/modals/VoirFactureModal.vue
+++ gamma2024.client/src/components/modals/VoirFactureModal.vue
@@ -1,6 +1,6 @@
 <template lang="">
     <div class="modal fade"
-         :id="'modal' + props.idFacture"
+         :id="'modalFacture' + props.idFacture"
          data-bs-backdrop="static"
          data-bs-keyboard="false"
          tabindex="-1"
@@ -15,10 +15,68 @@
                             aria-label="Close"></button>
                 </div>
                 <div class="modal-body d-flex justify-content-center h-100">
-                    <div v-if="chargement">
-                        <p>Le PDF de votre facture est présentement en génération. Veuillez revenir plus tard.</p>
+                    <div class="container my-2">
+                        <div class="d-flex align-items-center">
+                            <h5 class="col-9">Les Encans de Nantes au Québec - FACTURE</h5>
+                            <div class="col-3">
+                                <img class="img-fluid" src="https://sqlinfocg.cegepgranby.qc.ca/2162067/images/Logo.png" />
+                            </div>
+                        </div>
+                        <hr />
+                        <div class="d-flex">
+                            <div class="d-flex flex-column col-6 justify-content-between">
+                                <h5>Facturer à</h5>
+                                <span>{model.Client.Nom}</span>
+                                <span>{model.Client.AdresseLigne1}</span>
+                                <span>{model.Client.AdresseLigne2}</span>
+                                <span>{model.Client.AdresseLigne3}</span>
+                                <span>{model.Client.CodePostal}</span>
+                                <span>{model.Client.Courriel}</span>
+                                <span>{model.Client.Telephone}</span>
+                            </div>
+                            <div class="col-6 d-flex justify-content-end">
+                                <h5>{model.DateAchat}</h5>
+                            </div>
+                        </div>
+                        <br />
+                        <h3 class="text-center">
+                            {model.PrixFinal} $ payé le {model.DateAchat}
+                        </h3>
+                        <div class="d-flex w-100">
+                            <table class="table">
+                                <tbody id="tableProduits">
+                                    <tr>
+                                        <th scope="col" class="text-start">Frais de livraison</th>
+                                        <td scope="col" class="text-end">{model.FraisLivraison} $</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <br />
+                        <div class="d-flex flex-column">
+                            <table class="table col-6">
+                                <tbody>
+                                    <tr>
+                                        <th scope="col" class="text-start">Sous-total</th>
+                                        <td scope="col" class="text-end">{model.SousTotal} $</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="col" class="text-start">TPS</th>
+                                        <td scope="col" class="text-end">{model.TPS} $</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="col" class="text-start">TVQ</th>
+                                        <td scope="col" class="text-end">{model.TVQ} $</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="col" class="text-start">Don</th>
+                                        <td scope="col" class="text-end">{model.Don} $</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <h4>TOTAL: {model.PrixFinal} $</h4>
+                        </div>
                     </div>
-                    <iframe :src="pdf" class="w-100 min-vh-50" scrolling="no" v-else />
                 </div>
             </div>
         </div>
@@ -32,22 +90,13 @@
     const store = useStore();
 
     const props = defineProps({
-        facturePdfPath: String,
         idFacture: String
     });
 
-    const pdf = ref("")
-    const chargement = ref(true)
+    const facture = ref("")
 
     onMounted(async () => {
-        const response = await store.state.api.defaults.avatarURL
-        console.log(response)
-        pdf.value = response + "/" + props.facturePdfPath
-        console.log(pdf.value)
 
-        if (pdf.value != response) {
-            chargement.value = false
-        }
     });
 </script>
 <style scoped>
