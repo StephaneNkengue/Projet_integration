2349d363e35a1cd238c0be3b490429d0bde86291
AdaNeil <2135621@CegepGranby.qc.ca>
2024-12-08 15:23:10 -0500

    Ajout de VendeurLotsInfo.cs afin de l'utiliser dans le seeder

---
+++ Gamma2024.Server/Extensions/Objets/VendeurLotsInfo.cs
@@ -0,0 +1,9 @@
+namespace Gamma2024.Server.Extensions.Objets
+{
+    public class VendeurLotsInfo
+    {
+        public int Ligne { get; set; }
+        public List<string> NumerosLots { get; set; } = default!;
+        public int NumeroEncan { get; set; }
+    }
+}
